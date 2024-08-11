4cfd07569d4aaa2c3b52cd82024cba7fcafc87c8
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 01:45:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import React, { useEffect } from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 import Projects from './pages/Projects'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
-import axios from 'axios'
+import RedirectIfNeeded from './RedirectIfNeeded'
 
 function App() {
-
-  useEffect(() => {
-    const email = localStorage.getItem('upfront_user') || ''
-
-    const verifyUser = async () => {
-      // if (email !==)
-      try {
-        const response = await axios.get('http://localhost:5000/api/verify', { email })
-      }
-      catch (error) {
-        console.log(error)
-      }
-    }
-
-    verifyUser()
-  }, [])
-
   return (
     <>
       <HashRouter>
+        <RedirectIfNeeded />
         <Routes>
           <Route path='/' element={<Projects />} />
           <Route path='/auth/login' element={<Login />} />
