1fe6505dc2aede87842596fa42bf3a2d54edf1ef
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-11 18:24:13 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,12 @@ import logo from '../assets/logo-60x60.png'
 function Login() {
   return (
     <div className='w-full h-fit min-h-svh flex flex-col'>
-        <div className='w-full border-b border-container-color py-3'>
-            <div className='flex items-center gap-2'>
+        <div className='w-full border-b border-container-color py-3 px-10 '>
+            <div className='flex items-center gap-1'>
                 <div className='min-w-8'>
                   <img src={logo} className="h-8" loading='lazy' />
                 </div>
-                <h1 className='font-bold text-xl text-main-color'>Upfront</h1>
+                <h1 className='font-semibold text-xl font-l text-main-color tracking-tight'>Upfront.</h1>
             </div>
         </div>
     </div>
