1f7db323a3a51a92a3d24e09480fcef291824871
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-11 19:29:34 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,19 +18,20 @@ function Login() {
       <div className="w-full max-w-[400px] p-5 mx-auto h-full flex-1 flex flex-col items-start justify-center gap-2">
         <div className='w-full'>
           <h1 className='text-2xl font-semibold'>Login</h1>
-          <p className='font-medium opacity-70 pb-4 text-xs'>Hi, Welcome back!</p>
+          <p className='font-medium text-text-color/70 pb-4 text-xs'>Hi, Welcome back!</p>
           <button title='Login with Google' className='w-full h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
             Login with Google
             <FcGoogle className='text-xl' />
           </button>
         </div>
-        <h1 className='relative w-full py-3'>
+        <h1 className='relative w-full py-4'>
           <hr className='border-border-line-color z-10' />
-          <span className='absolute top-0 left-0 bottom-0 right-0 m-auto w-fit h-fit bg-white text-xs px-3 font-medium opacity-70 '>or login with Email</span>
+          <span className='absolute top-0 left-0 bottom-0 right-0 m-auto w-fit h-fit bg-white text-xs px-3 font-medium text-text-color/70 '>or login with Email</span>
         </h1>
         <form className="flex flex-col items-start justify-start gap-2 w-full h-fit ">
-        <label>
-          <input type="text" name="" id="" />
+        <label className='w-full'>
+          <h1>Email</h1>
+          <input type="text" className="w-full h-[40px] ring-1 ring-border-line-color p-3" id="" />
         </label>
         </form>        
       </div>
