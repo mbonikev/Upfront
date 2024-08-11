e2653ebe26601ea29411ffeba9b33308a8281ecb
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-11 18:01:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Projects from './assets/pages/Projects'
+import Login from './assets/pages/Login'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Projects />}/>
+          <Route path='/auth/login' element={<Login />}/>
+        </Routes>
+      </HashRouter>
+    </>
   )
 }
 
