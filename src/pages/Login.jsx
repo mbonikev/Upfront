42abd1dbc85856ec568944c588b5abf4707280d6
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-11 19:26:46 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logo from '../assets/logo-60x60.png'
+import { FcGoogle } from 'react-icons/fc'
 
 function Login() {
   return (
-    <div className='w-full h-fit min-h-svh flex flex-col text-sm'>
+    <div className='w-full h-fit min-h-svh flex flex-col text-sm text-text-color'>
       {/* topbar */}
       <div className='w-full py-3 px-10 '>
         <div className='flex items-center gap-1'>
@@ -14,16 +15,25 @@ function Login() {
         </div>
       </div>
       {/* form */}
-      <form className="w-full max-w-[400px] p-5 mx-auto h-full flex-1 flex flex-col items-start justify-center">
+      <div className="w-full max-w-[400px] p-5 mx-auto h-full flex-1 flex flex-col items-start justify-center gap-2">
         <div className='w-full'>
           <h1 className='text-2xl font-semibold'>Login</h1>
-          <p className='font-medium opacity-70 pb-4'>Hi, Welcome back!</p>
-          <button className='w-full h-[45px] ring-1 ring-border-line-color rounded-md font-semibold'>Login with Google</button>
+          <p className='font-medium opacity-70 pb-4 text-xs'>Hi, Welcome back!</p>
+          <button title='Login with Google' className='w-full h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
+            Login with Google
+            <FcGoogle className='text-xl' />
+          </button>
         </div>
+        <h1 className='relative w-full py-3'>
+          <hr className='border-border-line-color z-10' />
+          <span className='absolute top-0 left-0 bottom-0 right-0 m-auto w-fit h-fit bg-white text-xs px-3 font-medium opacity-70 '>or login with Email</span>
+        </h1>
+        <form className="flex flex-col items-start justify-start gap-2 w-full h-fit ">
         <label>
           <input type="text" name="" id="" />
         </label>
-      </form>
+        </form>        
+      </div>
     </div>
   )
 }
