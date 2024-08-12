5a92cfc593c609bc1d0e0455f70b701cd32a3e78
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 12:36:17 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import RedirectIfNeeded from '../contexts/RedirectIfNeeded'
 
 function Projects() {
+  const handleLogout = () => {
+    localStorage.removeItem('upfront_user')
+    window.location.reload()
+  }
   return (
-    <div>Projects</div>
+    <div>
+        <RedirectIfNeeded />
+        projects
+        <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
+            Logout
+          </button>
+    </div>
   )
 }
 
