a753d12516f666aecf79fffda5568526cde3c353
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 16:45:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import ProtectedRoutes from './utils/ProtectiveRoutes'
 import ProtectedAuthRoutes from './utils/ProtectedAuthRoutes'
+import ForgotPassword from './pages/ForgotPassword'
+import FPQ from './pages/FPQ'
+import CreateNewPassword from './pages/CreateNewPassword'
+import PasswordChangedSuccessfully from './pages/PasswordChangedSuccessfully'
+import ProtectedFP from './utils/ProtectedFP'
 
 function App() {
   return (
@@ -20,6 +25,13 @@ function App() {
           <Route element={<ProtectedAuthRoutes />}>
             <Route path='/auth/login' element={<Login />} />
             <Route path='/auth/signup' element={<SignUp />} />
+            <Route path='/forgotPassword' element={<ForgotPassword />} />
+            <Route path='/success' element={<PasswordChangedSuccessfully />} />
+          </Route>
+          {/* PR */}
+          <Route element={<ProtectedFP />}>
+            <Route path='/fpq' element={<FPQ />} />
+            <Route path='/newPassword' element={<CreateNewPassword />} />
           </Route>
         </Routes>
       </HashRouter>
