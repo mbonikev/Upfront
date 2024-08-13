caef0b0ad089eeed0e1e4fb37f35d005898e40e2
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-13 18:41:41 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { BiSolidGridAlt } from 'react-icons/bi'
 import { BsGrid, BsViewStacked } from 'react-icons/bs'
 import { CiGrid2H, CiGrid41 } from 'react-icons/ci'
-import { FaPlus } from 'react-icons/fa'
+import { FaPlus, FaRegStar } from 'react-icons/fa'
 import { FiPlus } from 'react-icons/fi'
 import { HiOutlineViewGridAdd, HiViewGrid } from 'react-icons/hi'
 import { IoMdNotificationsOutline } from 'react-icons/io'
-import { IoAdd, IoAddCircle, IoAddOutline } from 'react-icons/io5'
-import { MdNotifications, MdViewStream } from 'react-icons/md'
+import { IoAdd, IoAddCircle, IoAddOutline, IoSearchOutline, IoStarOutline } from 'react-icons/io5'
+import { MdFormatListBulleted, MdNotifications, MdViewStream } from 'react-icons/md'
 import { RiNotification4Fill } from 'react-icons/ri'
 import { RxTimer, RxViewGrid, RxViewHorizontal } from 'react-icons/rx'
+import { TiThList } from 'react-icons/ti'
 import { useOutletContext } from 'react-router-dom'
 
 function Projects() {
@@ -24,8 +25,11 @@ function Projects() {
   return (
     <div className='w-full h-full min-h-svh text-text-color flex flex-col'>
       <div className='w-full h-fit flex items-center justify-between px-10 py-5'>
-        <div className='flex flex-col items-start justify-start gap-2'>
-        <h1 className='text-3xl font-extrabold tracking-tight'>Projects</h1>
+        <div className='flex items-center justify-start gap-1 '>
+          <div className='h-fit w-fit transition hover:bg-stone-200 select-none flex items-center justify-center p-1 rounded-lg cursor-pointer'>
+          <p className='text-2xl bgora'>💼</p>
+          </div>
+          <h1 className='text-3xl font-extrabold tracking-tight'>Projects</h1>
         </div>
         <div className='flex items-center justify-end gap-2'>
           <button title='create a new project' className="bg-stone-100 hover:bg-main-color-hover transition hover:text-white text-sm font-semibold py-[8px] px-4 gap-1 rounded-full inline-flex items-center">
@@ -33,7 +37,7 @@ function Projects() {
             <span className='text-xs'>New Project</span>
           </button>
           <button title='Deadlines' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
-            <RxTimer  />
+            <RxTimer />
           </button>
           <button title='Notifications' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
             <IoMdNotificationsOutline />
@@ -42,15 +46,25 @@ function Projects() {
         </div>
       </div>
 
-      <div className='w-full h-full flex-1 px-10 pb-10'>
-      <p className='font-medium opacity-65'>20 incomplete Projects</p>
-      <span className='w-full h-[1px] bg-stone-100 flex mt-2 '></span>
-
+      {/* Projects section */}
+      <div className='w-full h-full flex-1 px-10 py-6'>
+        <div className='w-full h-fit flex items-center justify-between'>
+          <p className='font-medium text-sm text-text-color/60'><span className='text-text-color font-bold'>20</span> Open Projects | <span className='text-text-color font-bold'>20</span>  Closed Projects</p>
+          <div className='flex items-center justify-end gap-0'>
+            <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
+              <IoSearchOutline />
+            </button>
+            <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
+              <IoStarOutline />
+            </button>
+          </div>
+        </div>
+        <span className='w-full h-[1px] bg-stone-100 flex mt-2 '></span>
       </div>
 
       <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
-            Logout
-          </button>
+        Logout
+      </button>
     </div>
   )
 }
