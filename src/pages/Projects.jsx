e873faedd13ec4e71dd6d41ee9d8fc515d0cb6e6
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-14 00:13:20 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -99,87 +99,98 @@ function Projects() {
   };
 
   return (
-    <div className='w-full h-full min-h-svh text-text-color flex flex-col'>
-      <div className='w-full h-fit flex items-start justify-between px-10 py-5'>
-        <div className='flex items-end justify-start gap-0 '>
-          <div className='group h-fit w-fit transition hover:bg-stone-100 select-none relative flex items-center justify-center p-1 mr-1 rounded-lg cursor-pointer'>
-            <p className='text-2xl bgora'>{pemoji}</p>
-            <Emojis change={ChangeEmoji} />
+    <div className='w-full flex items-start justify-start'>
+      <div className='w-[300px] h-full min-h-svh border-r-[1px] border-border-line-color/20 bg-sidebar-color'></div>
+      <div className='w-full h-full min-h-svh text-text-color flex flex-col'>
+        <div className='w-full h-fit flex items-start justify-between px-5 pt-5'>
+          <div className='flex items-end justify-start gap-0 '>
+            
+            {/* <div className='group h-fit w-fit transition hover:bg-stone-100 select-none relative flex items-center justify-center p-1 mr-1 rounded-lg cursor-pointer'>
+              <p className='text-2xl bgora'>{pemoji}</p>
+              <Emojis change={ChangeEmoji} />
+            </div> */}
+            {/* growing input */}
+            {/* <input
+              ref={inputRef}
+              type="text"
+              value={pageTitle}
+              onChange={(e) => setPageTitle(e.target.value)}
+              placeholder="Type here.."
+              maxLength={20}
+              className='text-3xl font-extrabold tracking-tight hover:ring-2 ring-slate-400/40 rounded-md'
+            /> */}
+            {/* <h1 className='text-3xl font-extrabold tracking-tight'>Projects</h1> */}
+            {/* <span className=' self-end text-xs font-medium bg-teal-600/10 mb-[4px] ml-1 py-[3px] px-2 tracking-tight rounded-md'>Free</span> */}
           </div>
-          {/* growing input */}
-          <input
-            ref={inputRef}
-            type="text"
-            value={pageTitle}
-            onChange={(e) => setPageTitle(e.target.value)}
-            placeholder="Type here.."
-            maxLength={20}
-            className='text-3xl font-extrabold tracking-tight hover:ring-2 ring-slate-400/40 rounded-md'
-          />
-          <span className=' self-end text-xs font-medium bg-teal-600/10 mb-[4px] ml-1 py-[3px] px-2 tracking-tight rounded-md'>Free</span>
+          <div className='flex items-center justify-end gap-2'>
+            <button title='create a new project' className="bg-main-color hover:bg-main-color-hover transition text-white text-xs font-semibold py-[8px] px-5 gap-1 rounded-full inline-flex items-center">
+              <FaPlus />
+              <span className='text-xs font-bold'>Create</span>
+            </button>
+            {/* <button title='Deadlines' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
+              <RxTimer />
+            </button> */}
+            {/* <button title='Notifications' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
+              <IoMdNotificationsOutline />
+            </button> */}
+            {/* <button title={username} className='h-[35px] w-auto aspect-square rounded-full bg-main-color hover:bg-main-color-hover transition flex items-center justify-center text-lg font-semibold text-white'>{username.charAt(0)}</button> */}
+          </div>
         </div>
-        <div className='flex items-center justify-end gap-2'>
-          <button title='create a new project' className="bg-stone-100 hover:bg-main-color-hover transition hover:text-white text-sm font-semibold py-[8px] px-4 gap-1 rounded-full inline-flex items-center">
-            <FaPlus />
-            <span className='text-xs'>New Project</span>
-          </button>
-          <button title='Deadlines' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
-            <RxTimer />
-          </button>
-          <button title='Notifications' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-stone-100 hover:bg-stone-200'>
-            <IoMdNotificationsOutline />
-          </button>
-          <button title={username} className='h-[35px] w-auto aspect-square rounded-full bg-main-color hover:bg-main-color-hover transition flex items-center justify-center text-lg font-semibold text-white'>{username.charAt(0)}</button>
+        <div className='w-full h-fit flex items-start justify-between px-16 py-5'>
+          <div className='flex items-end justify-start gap-1 '>
+            <h1 className='text-3xl font-extrabold tracking-tight'>Projects</h1>
+            <span className=' self-end text-xs font-medium bg-teal-600/10 mb-[4px] ml-1 py-[3px] px-2 tracking-tight rounded-md'>Free</span>
+          </div>
         </div>
-      </div>
 
-      {/* Projects section */}
-      <div className='w-full h-full flex-1 bg-white px-10 py-6'>
-        <div className='w-full h-fit flex items-center justify-between'>
-          <p className='font-medium text-sm text-text-color/60'><span className='text-text-color font-bold'>20</span> in Progress | <span className='text-text-color font-bold'>20</span>  Completed</p>
-          <div className='flex items-center justify-end gap-0'>
-            <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
-              <IoSearchOutline />
-            </button>
-            <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
-              <IoStarOutline />
-            </button>
+        {/* Projects section */}
+        <div className='w-full h-full flex-1 bg-white px-16 pb-10'>
+          <div className='w-full h-fit flex items-end justify-between'>
+            <p className='font-normal text-[13px] text-text-color/70'><span className='text-text-color font-medium'>20</span> in Progress | <span className='text-text-color font-medium'>20</span>  Completed</p>
+            <div className='flex items-center justify-end gap-0'>
+              <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
+                <IoSearchOutline />
+              </button>
+              <button title='search' className='text-xl h-[35px] w-auto aspect-square flex items-center justify-center rounded-full transition bg-transparent hover:bg-stone-200'>
+                <IoStarOutline />
+              </button>
+            </div>
           </div>
-        </div>
-        <span className='w-full h-[1px] bg-border-line-color/50 flex mt-2 '></span>
-        <div className='gridRespo pt-3'>
-          {projects.map((project, index) => (
-            <Link key={index} to={'/'} className='w-full max-w-[350px] max-md:max-w-full h-fit p-4 rounded-xl shadow-sm bg-white transition hover:ring-2 hover:ring-main-color/60 ring-1 ring-border-line-color/50 flex flex-col'>
-              <h1 className='font-normal text-lg leading-7'> Gerayo Application</h1>
-              <p className='line-clamp-2 leading-4 text-sm font-normal text-text-color/70'>Online Bus tracking and Ticketing system</p>
-              <div className='flex items-center justify-start mt-3'>
-                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-orange-600 text-white text-base font-semibold border-[3px] border-white'>J</div>
-                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-teal-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>E</div>
-                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-purple-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>I</div>
-                <div className='flex items-center justify-center text-xs text-text-color/70 px-[2px] font-medium'>+2</div>
-              </div>
-              <div className='flex items-center gap-3 py-3'>
-                <h1 className='text-sm'>{project.progress}%</h1>
-                <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                  <div className={`flex flex-col justify-center rounded-full overflow-hidden bg-main-color/70 text-xs text-white text-center whitespace-nowrap transition duration-500 ${getProgressClasses(project.progress)} ${project.progressClass}`}></div>
+          <span className='w-full h-[1px] bg-border-line-color/50 flex mt-2 '></span>
+          <div className='gridRespo pt-4'>
+            {projects.map((project, index) => (
+              <Link key={index} to={'/'} className='w-full max-w-[350px] max-md:max-w-full h-fit p-4 rounded-xl shadow-sm bg-white transition hover:ring-2 hover:ring-main-color/60 ring-1 ring-border-line-color/50 flex flex-col'>
+                <h1 className='font-normal text-sm leading-7'> Gerayo Application</h1>
+                <p className='line-clamp-1 leading-4 text-sm font-normal text-text-color/70'>Online Bus tracking and Ticketing system</p>
+                <div className='flex items-center justify-start mt-3'>
+                  <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-orange-600 text-white text-base font-semibold border-[3px] border-white'>J</div>
+                  <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-teal-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>E</div>
+                  <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-purple-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>I</div>
+                  <div className='flex items-center justify-center text-xs text-text-color/70 px-[2px] font-medium'>+2</div>
                 </div>
-                <h1 className='text-sm'>100%</h1>
-              </div>
-              <div className='w-full flex items-center justify-between'>
-                <p className='w-full flex items-center justify-start text-xs gap-1 font-medium text-text-color/70'>
-                  <TbStack className='text-xl' />
-                  {project.progress} Boards
-                </p>
-                <p className='w-full flex items-start justify-end text-xs font-medium text-text-color/70'>May 11, 2023</p>
-              </div>
-            </Link>
-          ))}
+                <div className='flex items-center gap-3 py-3'>
+                  <h1 className='text-sm'>{project.progress}%</h1>
+                  <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                    <div className={`flex flex-col justify-center rounded-full overflow-hidden bg-main-color/70 text-xs text-white text-center whitespace-nowrap transition duration-500 ${getProgressClasses(project.progress)} ${project.progressClass}`}></div>
+                  </div>
+                  <h1 className='text-sm'>100%</h1>
+                </div>
+                <div className='w-full flex items-center justify-between'>
+                  <p className='w-full flex items-center justify-start text-xs gap-1 font-medium text-text-color/70'>
+                    <TbStack className='text-xl' />
+                    {project.progress} Boards
+                  </p>
+                  <p className='w-full flex items-start justify-end text-xs font-medium text-text-color/70'>May 11, 2023</p>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
+        {/* <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
         Logout
       </button> */}
+      </div>
     </div>
   )
 }
