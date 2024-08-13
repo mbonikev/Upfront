635e233c92938cb223b6ba34639f500bb2866f7a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-13 21:15:31 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,7 +19,8 @@ function Projects() {
   const { username } = useOutletContext()
   const [pemoji, setPemoji] = useState(null)
   const projects = [
-    "", 
+    {progress: "25%" ,progressClass: "w-[25%]"},{progress: "64%" ,progressClass: "w-[64%]"},{progress: "34%" ,progressClass: "w-[34%]"},{progress: "58%" ,progressClass: "w-[58%]"},{progress: "19%" ,progressClass: "w-[19%]"},
+    {progress: "81%" ,progressClass: "w-[81%]"},{progress: "92%" ,progressClass: "w-[92%]"},{progress: "44%" ,progressClass: "w-[44%]"},{progress: "8%" ,progressClass: "w-[8%] "},{progress: "69%" ,progressClass: "w-[69%] "},
   ]
 
   const handleLogout = () => {
@@ -65,7 +66,7 @@ function Projects() {
             <Emojis change={ChangeEmoji} />
           </div> */}
           <h1 className='text-3xl font-extrabold tracking-tight'>Workspace 1</h1>
-          <span className=' self-end text-xs bg-orange-500/10 mb-1 ml-1 py-1 px-2 tracking-tight rounded-md'>Free Plan</span>
+          <span className=' self-end text-xs bg-main-color/5 mb-[4px] ml-1 py-1 px-2 tracking-tight rounded-md'>Free</span>
         </div>
         <div className='flex items-center justify-end gap-2'>
           <button title='create a new project' className="bg-stone-100 hover:bg-main-color-hover transition hover:text-white text-sm font-semibold py-[8px] px-4 gap-1 rounded-full inline-flex items-center">
@@ -83,7 +84,7 @@ function Projects() {
       </div>
 
       {/* Projects section */}
-      <div className='w-full h-full flex-1 px-10 py-6'>
+      <div className='w-full h-full flex-1 bg-white px-10 py-6'>
         <div className='w-full h-fit flex items-center justify-between'>
           <p className='font-medium text-sm text-text-color/60'><span className='text-text-color font-bold'>20</span> Open Projects | <span className='text-text-color font-bold'>20</span>  Closed Projects</p>
           <div className='flex items-center justify-end gap-0'>
@@ -95,10 +96,10 @@ function Projects() {
             </button>
           </div>
         </div>
-        <span className='w-full h-[1px] bg-stone-100 flex mt-2 '></span>
+        <span className='w-full h-[1px] bg-border-line-color/50 flex mt-2 '></span>
         <div className='gridRespo pt-3'>
           {projects.map((project, index) => (
-            <Link key={index} to={'/'} className='w-full max-w-[350px] max-md:max-w-full h-fit p-4 rounded-xl shadow-xl shadow-border-line-color/10 bg-white transition hover:ring-2 hover:ring-stone-200 ring-1 ring-border-line-color/50 flex flex-col'>
+            <Link key={index} to={'/'} className='w-full max-w-[350px] max-md:max-w-full h-fit p-4 rounded-xl shadow-sm bg-white transition hover:ring-2 hover:ring-main-color/60 ring-1 ring-border-line-color/50 flex flex-col'>
               <h1 className='font-normal text-lg leading-7'> Gerayo Application</h1>
               <p className='line-clamp-2 leading-4 text-sm font-normal text-text-color/70'>Online Bus tracking and Ticketing system</p>
               <div className='flex items-center justify-start mt-3'>
@@ -107,15 +108,22 @@ function Projects() {
                 <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-purple-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>I</div>
                 <div className='flex items-center justify-center text-sm px-[2px] font-medium'>+29</div>
               </div>
+              <div className='flex items-center gap-3 py-3'>
+                <h1 className='text-sm'>{project.progress}</h1>
+                <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                  <div className={`flex flex-col justify-center rounded-full overflow-hidden bg-main-color/70 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500 ${project.progressClass}`}></div>
+                </div>
+                <h1 className='text-sm'>100%</h1>
+              </div>
               <p className='w-full flex items-start justify-end text-xs font-medium text-text-color/70'>May 11, 2023</p>
             </Link>
           ))}
         </div>
       </div>
 
-      <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
+      {/* <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
         Logout
-      </button>
+      </button> */}
     </div>
   )
 }
