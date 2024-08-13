2d542de92625ddd03d60cab5d7b2fc6834ca6e1e
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-13 20:29:29 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,13 +11,16 @@ import { MdFormatListBulleted, MdNotifications, MdViewStream } from 'react-icons
 import { RiNotification4Fill } from 'react-icons/ri'
 import { RxTimer, RxViewGrid, RxViewHorizontal } from 'react-icons/rx'
 import { TiThList } from 'react-icons/ti'
-import { useOutletContext } from 'react-router-dom'
+import { Link, useOutletContext } from 'react-router-dom'
 import Emojis from '../components/Emojis'
 import { EmojiArray } from '../content/data'
 
 function Projects() {
   const { username } = useOutletContext()
   const [pemoji, setPemoji] = useState(null)
+  const projects = [
+    "", 
+  ]
 
   const handleLogout = () => {
     localStorage.removeItem('upfront_user')
@@ -38,7 +41,7 @@ function Projects() {
   useEffect(() => {
     // Initial update
     updateEmoji();
-     
+
     // Add event listener for localStorage changes from other tabs
     window.addEventListener('storage', updateEmoji);
 
@@ -51,7 +54,7 @@ function Projects() {
   const ChangeEmoji = (position) => {
     localStorage.setItem('projectsmoji', position)
     updateEmoji()
-}
+  }
 
   return (
     <div className='w-full h-full min-h-svh text-text-color flex flex-col'>
@@ -61,7 +64,8 @@ function Projects() {
             <p className='text-2xl bgora'>{pemoji}</p>
             <Emojis change={ChangeEmoji} />
           </div> */}
-          <h1 className='text-3xl font-extrabold tracking-tight'>Projects</h1>
+          <h1 className='text-3xl font-extrabold tracking-tight'>Workspace 1</h1>
+          <span className=' self-end text-xs bg-orange-500/10 mb-1 ml-1 py-1 px-2 tracking-tight rounded-md'>Free Plan</span>
         </div>
         <div className='flex items-center justify-end gap-2'>
           <button title='create a new project' className="bg-stone-100 hover:bg-main-color-hover transition hover:text-white text-sm font-semibold py-[8px] px-4 gap-1 rounded-full inline-flex items-center">
@@ -92,6 +96,21 @@ function Projects() {
           </div>
         </div>
         <span className='w-full h-[1px] bg-stone-100 flex mt-2 '></span>
+        <div className='gridRespo pt-3'>
+          {projects.map((project, index) => (
+            <Link key={index} to={'/'} className='w-full max-w-[350px] max-md:max-w-full h-fit p-4 rounded-xl shadow-xl shadow-border-line-color/10 bg-white transition hover:ring-2 hover:ring-stone-200 ring-1 ring-border-line-color/50 flex flex-col'>
+              <h1 className='font-normal text-lg leading-7'> Gerayo Application</h1>
+              <p className='line-clamp-2 leading-4 text-sm font-normal text-text-color/70'>Online Bus tracking and Ticketing system</p>
+              <div className='flex items-center justify-start mt-3'>
+                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-orange-600 text-white text-base font-semibold border-[3px] border-white'>J</div>
+                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-teal-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>E</div>
+                <div className='h-8 w-auto aspect-square rounded-full flex items-center justify-center bg-purple-600 text-white text-base font-semibold ml-[-9px] border-[3px] border-white'>I</div>
+                <div className='flex items-center justify-center text-sm px-[2px] font-medium'>+29</div>
+              </div>
+              <p className='w-full flex items-start justify-end text-xs font-medium text-text-color/70'>May 11, 2023</p>
+            </Link>
+          ))}
+        </div>
       </div>
 
       <button onClick={handleLogout} title='Login with Google' className='w-[200px] h-[40px] ring-1 ring-border-line-color rounded-md font-semibold flex items-center justify-center gap-1 transition hover:opacity-80'>
