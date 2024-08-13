8e194d877a5fa6412ac5514eee5ad459085a0527
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-14 00:22:30 +0200

    Create BreadCrumb.jsx

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function BreadCrumb({name ,link ,status}) {
+  return (
+    <>
+        {status === 'on' ? (
+            <Link className='py-1 px-2 text-xs font-medium hover:bg-stone-100 rounded-md text-text-color/70 cursor-pointer' to={link}>{name}</Link>
+        ) : (
+            <button className='py-1 px-2 text-xs font-medium text-text-color/70 cursor-default'>{name}</button>
+        )}
+    </>
+  )
+}
+
+export default BreadCrumb
