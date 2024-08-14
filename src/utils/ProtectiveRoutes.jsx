46418b94fcef618280f5423e1394e97e4ada2be0
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-14 15:45:19 +0200

    Update ProtectiveRoutes.jsx

--- src/utils/ProtectiveRoutes.jsx
+++ src/utils/ProtectiveRoutes.jsx
@@ -4,6 +4,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const ProtectedRoutes = () => {
     const [user, setUser] = useState(null); // Initialize with null
     const [username, setUserName] = useState(null); // Initialize with null
+    const [userEmail, setUserEmail] = useState(null)
 
     useEffect(() => {
         const email = localStorage.getItem('upfront_user');
@@ -11,9 +12,11 @@ const ProtectedRoutes = () => {
         if (email) {
             setUser(true);
             setUserName(name)
+            setUserEmail(email)
         } else {
             setUser(false);
-            setUserName('')
+            setUserName(null)
+            setUserEmail(null)
         }
     }, []);
 
@@ -22,7 +25,7 @@ const ProtectedRoutes = () => {
         return <></>;
     }
 
-    return user ? <Outlet context={{ username }} /> : <Navigate to={"/auth/login"} />;
+    return user ? <Outlet context={{ username, userEmail }} /> : <Navigate to={"/auth/login"} />;
 };
 
 export default ProtectedRoutes;
