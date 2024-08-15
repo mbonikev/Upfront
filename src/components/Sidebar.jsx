f4a4ad0913aafeb211fea63d5be3df1acf0a33da
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-15 17:12:12 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -185,10 +185,10 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1, w2, setW
                 )}
                 <div className='w-full h-fit min-h-svh max-h-svh border-r-[1px] border-border-line-color/20 bg-sidebar-color flex flex-col gap-[2px] p-3 text-sm overflow-y-auto'>
                     <div className='w-full flex items-center justify-between mb-4'>
-                        <button onClick={showPMenu} className=' max-w-[105px] flex items-center justify-start gap-[2px] hover:bg-stone-200 transition p-1 rounded-lg'>
+                        <button onClick={showPMenu} className='text-text-color max-w-[150px] flex items-center justify-start gap-[2px] hover:bg-stone-200 transition p-1 rounded-lg'>
                             <p className='h-[26px] w-auto aspect-square rounded-full bg-main-color hover:bg-main-color-hover transition flex items-center justify-center text-base font-semibold text-white'>{username.charAt(0)}</p>
-                            <p className='truncate font-medium pl-[6px]'>{username}</p>
-                            <IoChevronDown className='min-w-[15px] text-text-color/70' />
+                            <p className='truncate font-medium text-sm tracking-tight pl-[6px]'>{username}</p>
+                            <IoChevronDown className='min-w-[15px] ' />
                         </button>
                         <div className='flex items-center justify-end gap-0'>
 
