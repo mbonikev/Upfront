e3ff88f4e5441c647c649e8123d3ad824058fefd
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-15 15:31:07 +0200

    Update Emojis.jsx

--- src/components/Emojis.jsx
+++ src/components/Emojis.jsx
@@ -5,7 +5,7 @@ function Emojis({ change }) {
     
 
     return (
-        <div className='bg-transparent pt-3 absolute top-[100%] left-0 w-[350px] h-fit hidden group-hover:flex mr-3 '>
+        <div className='bg-transparent pt-2 absolute top-[100%] left-0 w-[350px] h-fit hidden group-hover:flex mr-3 '>
             <div className='h-full w-full max-h-[400px] overflow-auto p-2 rounded-lg bg-white shadow-xl ring-1 ring-border-line-color/70 grid grid-cols-8 custom-scrollbar'>
                 {EmojiArray.map((emoji, index) => (
                     <button key={index} onClick={() => change(emoji.position)} className='w-full h-auto aspect-square rounded-md hover:bg-stone-200 text-2xl'>
