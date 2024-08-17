13dabbd1257d1d404bded0468849cec51c8a2cf7
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-17 13:04:33 +0200

    Update AddCollaborators.jsx

--- src/components/AddCollaborators.jsx
+++ src/components/AddCollaborators.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { LuActivity, LuArchive, LuInfo, LuLogOut, LuSettings, LuTrash2, LuTrophy } from 'react-icons/lu'
+import { LuActivity, LuArchive, LuInfo, LuLogOut, LuSettings, LuTrash2, LuTrophy, LuUsers2, LuWorkflow } from 'react-icons/lu'
 import { RiLoader5Fill } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
@@ -20,8 +20,17 @@ function AddCollaborators({ username }) {
     }
     return (
         <div className='w-full flex flex-col justify-start items-start bg-white'>
+            <div className='pt-2 px-2 w-full'>
+                <div className='min-h-[34px] flex items-center gap-2 px-2 py-[3px] text-sm font-normal tracking-tight line-clamp-1 '>
+                    <LuUsers2 className='text-xl text-text-color/50 min-w-fit' />
+                    <div className='w-full h-fit flex-col'>
+                        <p className='line-clamp-1 text-sm font-medium text-text-color'>Add Collaborators</p>
+                        <p className='line-clamp-1 text-text-color/70 text-xs'>1 have access</p>
+                    </div>
+                </div>
+            </div>
             <form className='p-2 w-full'>
-                <input type='text' placeholder={`user's unique ID`} className='min-h-[34px] w-full flex items-center text-text-color gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md bg-stone-200/50 line-clamp-1 '/>
+                <input type='text' placeholder={`user's unique ID`} className='min-h-[34px] w-full flex items-center text-text-color gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md bg-stone-200/50 line-clamp-1 ' />
                 <button type='Submit' className='min-h-[34px] w-full mt-2 flex items-center justify-center gap-1 px-2 py-[7px] font-semibold text-sm tracking-tight rounded-md bg-main-color hover:bg-main-color-hover text-white line-clamp-1 '>
                     {/* <LuSettings className='text-xl min-w-fit' /> */}
                     <p className='line-clamp-1'>Find user</p>
@@ -53,7 +62,7 @@ function AddCollaborators({ username }) {
                     <p className='line-clamp-1'>How to Locate the Unique ID</p>
                 </Link>
             </div>
-            
+
         </div>
     )
 }
