58e9fae788b8e2d39693e27e17649d67f1ceae2e
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-17 10:46:53 +0200

    Update AddCollaborators.jsx

--- src/components/AddCollaborators.jsx
+++ src/components/AddCollaborators.jsx
@@ -22,6 +22,10 @@ function AddCollaborators({ username }) {
         <div className='w-full flex flex-col justify-start items-start bg-white'>
             <form className='p-2 w-full'>
                 <input type='text' placeholder={`user's unique ID`} className='min-h-[34px] w-full flex items-center text-text-color gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md bg-stone-200/50 line-clamp-1 '/>
+                <button type='Submit' className='min-h-[34px] w-full mt-2 flex items-center justify-center gap-1 px-2 py-[7px] font-semibold text-sm tracking-tight rounded-md bg-main-color hover:bg-main-color-hover text-white line-clamp-1 '>
+                    {/* <LuSettings className='text-xl min-w-fit' /> */}
+                    <p className='line-clamp-1'>Find user</p>
+                </button>
             </form>
             <div className='w-full h-[1px] bg-border-line-color/70'></div>
             <div className='p-2 flex flex-col w-full'>
@@ -46,7 +50,7 @@ function AddCollaborators({ username }) {
             <div className='p-2 flex flex-col w-full'>
                 <Link to={'/'} className='min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color text-sm tracking-tight rounded-md hover:bg-stone-200/50 line-clamp-1 '>
                     <LuInfo className='text-xl text-text-color/50  min-w-fit' />
-                    <p className='line-clamp-1'>How to find a Unique ID</p>
+                    <p className='line-clamp-1'>How to Locate the Unique ID</p>
                 </Link>
             </div>
             
