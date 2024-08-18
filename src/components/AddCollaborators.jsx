eb043dd9516398b48d78f74115590c8d96b43ed7
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-18 11:41:05 +0200

    Update AddCollaborators.jsx

--- src/components/AddCollaborators.jsx
+++ src/components/AddCollaborators.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { LuActivity, LuArchive, LuInfo, LuLogOut, LuSettings, LuTrash2, LuTrophy, LuUsers2, LuWorkflow } from 'react-icons/lu'
+import { LuActivity, LuArchive, LuAtSign, LuInfo, LuLogOut, LuSettings, LuTrash2, LuTrophy, LuUsers2, LuWorkflow } from 'react-icons/lu'
 import { RiLoader5Fill } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
@@ -29,11 +29,11 @@ function AddCollaborators({ username, userEmail, collaborations }) {
                 </div>
             </div>
             <form className='p-2 w-full'>
-                <input type='text' placeholder={`user's unique ID`} className='min-h-[34px] w-full flex items-center text-text-color gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md bg-stone-200/50 line-clamp-1 ' />
-                <button type='Submit' className='min-h-[34px] w-full mt-2 flex items-center justify-center gap-1 px-2 py-[7px] font-semibold text-sm tracking-tight rounded-md bg-main-color hover:bg-main-color-hover text-white line-clamp-1 '>
-                    {/* <LuSettings className='text-xl min-w-fit' /> */}
-                    <p className='line-clamp-1'>Find user</p>
-                </button>
+                <div className='flex items-center justify-start gap-2'>
+                    <div className='text-text-color/50 pl-2'><LuAtSign className='text-xl' /></div>
+                  <input type='text' placeholder={`user's unique ID`} className='min-h-[34px] w-full flex items-center text-text-color gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md bg-stone-200/50 line-clamp-1 ' />
+                </div>
+               
             </form>
             <div className='w-full h-[1px] bg-border-line-color/70'></div>
             <div className='p-2 flex flex-col w-full min-h-[140px]'>
