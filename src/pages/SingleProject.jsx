40709e119274131e428b6cd2537a7b9fdc67fb11
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-18 11:28:11 +0200

    Update SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -41,6 +41,7 @@ function SingleProject() {
   const [addBoard, setAddBoard] = useState(false)
   const [fetching, setFetching] = useState(true)
   const [fromSpace, setFromSpace] = useState('')
+  const [collaborations, setCollaborations ] = useState([])
   const location = useLocation()
   const { workspace } = location.state || {}
   // console.log(workspace)
@@ -86,6 +87,7 @@ function SingleProject() {
         setProjectDesc(response.data.desc)
         setFetching(false)
         setFromSpace(response.data.workspace)
+        setCollaborations(response.data.collaborations)
       }
       catch (error) {
         // console.log(error)
@@ -126,7 +128,7 @@ function SingleProject() {
 
       {userMenu && (
         <div className="w-[290px] h-fit max-h-[80vh] absolute top-[52px] right-[170px] rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50">
-          <AddCollaborators username={username} userEmail={userEmail} />
+          <AddCollaborators username={username} collaborations={collaborations} userEmail={userEmail} />
         </div>
       )}
 
