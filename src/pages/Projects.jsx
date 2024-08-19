1b8f17a23e9a2351389d0cb19a54395f60c94194
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-19 10:32:03 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -27,6 +27,9 @@ function Projects() {
   const [myCollaborations, setMyCollatorations] = useState([])
   const navigate = useNavigate()
   const [createNew, setCreateNew] = useState(false);
+  const [deleteMenu, setDeleteMenu] = useState('')
+  const [deleting, setDeleting] = useState(false)
+
 
 
   useEffect(() => {
@@ -161,13 +164,27 @@ function Projects() {
   // getting space names + naming the page
   useEffect(() => {
     const luw1 = localStorage.getItem('upfront_user_name_w1') || 'Workspace 1'
-    const luw2 = localStorage.getItem('upfront_user_name_w2') || 'Workspace 2'
-    const luw3 = localStorage.getItem('upfront_user_name_w3') || 'Workspace 3'
     setW1(luw1)
-    setW2(luw2)
-    setW3(luw3)
     document.title = luw1 + " - Upfront";
   }, [])
+
+  const showDeleteMenu = (id) => {
+    setDeleteMenu(id)
+  }
+
+  const handleTrashProject = async (id) => {
+    setDeleting(true)
+    try {
+      const response = await axios.post(`${apiUrl}/api/movetotrash`, {
+        projectId: id,
+        userEmail
+      });
+      window.location.reload()
+    } catch (err) {
+      console.log(err)
+      setDeleting(false)
+    }
+  }
 
   return (
     <div className='w-full flex items-start justify-start relative'>
@@ -244,14 +261,40 @@ function Projects() {
                   </button>
                 </div>
                 :
-                <div className='gridRespo pt-4 flex-1'>
+                <div className='gridRespo pt-4 flex-1 relative'>
+                  {/* delete Menu overlay */}
+                  <div
+                    onClick={() => setDeleteMenu(false)}
+                    className={` top-0 left-0 w-full h-full z-30 bg-transparent ${deleteMenu ? "absolute" : "hidden"
+                      }`}
+                  ></div>
                   {myProjects.map((project, index) => (
-                    <div key={index} className='group z-10 relative w-full lg:max-w-full xl:max-w-[500px] 2xl:max-w-[750px] h-fit'>
-                      <div className='absolute z-20 top-0 right-1 rounded-md flex items-center justify-center gap-0 bg-white p-1 opacity-0 group-hover:opacity-100 translate-y-1 group-hover:translate-y-0'>
-                        <button title='Delete Project' className={`h-[35px] w-auto aspect-square min-w-fit flex items-center justify-center gap-1 font-medium text-xs text-text-color/70 hover:text-red-500 tracking-tight rounded-full line-clamp-1 relative cursor-pointer hover:bg-stone-200/60 `}>
+                    <div key={index} className='group z-30 relative w-full lg:max-w-full xl:max-w-[500px] 2xl:max-w-[750px] h-fit'>
+                      <div className='absolute z-30 top-0 right-1 rounded-md flex items-center justify-center gap-0 bg-white p-1 opacity-0 group-hover:opacity-100 translate-y-1 group-hover:translate-y-0'>
+                        <button onClick={() => showDeleteMenu(project._id)} title='Delete Project' className={`h-[35px] w-auto aspect-square min-w-fit flex items-center justify-center gap-1 font-medium text-xs text-text-color/70 hover:text-red-500 tracking-tight rounded-full line-clamp-1 relative cursor-pointer hover:bg-stone-200/60 `}>
                           <LuTrash2 className='text-xl  min-w-fit ' />
                         </button>
                       </div>
+                      {/* Delete Dropdown */}
+                      {deleteMenu === project._id && (
+                        <div className="w-[290px] h-fit max-h-[80vh] p-2 absolute top-10 right-4 rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-[9999] bg-white">
+                          <p className="text-sm text-text-color/70 px-2 pt-2 pb-4"><span className="font-medium text-text-color">Warning! </span> Deleting this project will remove it from your workspace and move it to trash. Collaborations will be stashed for possible future restoration</p>
+                          <p className="text-sm text-text-color/70 px-2 pb-4"> Some fields, like <span className="font-medium text-text-color">'createdAt'</span> will reset during restore.</p>
+                          <div className="flex items-center justify-end">
+                            <button onClick={() => handleTrashProject(project._id)} title='Trash' className="bg-stone-100 hover:bg-red-500 hover:text-white transition text-xs font-semibold h-[35px] py-0 px-3 w-full gap-1 text-text-color/70 rounded-lg inline-flex items-center justify-center">
+                              {deleting ?
+                                <RiLoader5Fill className="text-2xl animate-spinLoader" />
+                                :
+                                <>
+                                  <LuTrash2 className='text-lg' />
+                                  <span className='text-sm font-medium tracking-tight'>Move to Trash</span>
+                                </>
+                              }
+
+                            </button>
+                          </div>
+                        </div>
+                      )}
                       <Link key={project._id} to={`/project/${project._id}`} className='group cursor-pointer w-full h-full p-4 rounded-xl shadow-sm bg-white group-hover:ring-2 group-hover:ring-main-color/60 ring-1 ring-border-line-color/50 flex flex-col relative'>
                         <h1 className='font-normal text-base leading-7 line-clamp-1'>{project.name === '' ? 'Untitled' : project.name}</h1>
                         <p className='line-clamp-1 leading-4 text-sm font-normal text-text-color/70 '>{project.name === '' ? 'no description' : project.desc}</p>
