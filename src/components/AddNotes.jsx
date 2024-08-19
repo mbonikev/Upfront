eb263e774eebccd1ad4b75133d90382966178859
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-19 09:41:32 +0200

    Create AddNotes.jsx

---
+++ src/components/AddNotes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import addnotes from '../assets/add_notes.svg'
+
+function AddNotes() {
+  return (
+    <img src={addnotes} alt="Add Notes" />
+)
+}
+
+export default AddNotes
