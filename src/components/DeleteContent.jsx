f0937e026dacd14502aaeb41e8e61b725084328a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-23 22:03:33 +0200

    Update DeleteContent.jsx

--- src/components/DeleteContent.jsx
+++ src/components/DeleteContent.jsx
@@ -3,18 +3,13 @@ import React, { useState } from 'react'
 import { LuTrash2 } from 'react-icons/lu';
 import { RiLoader5Fill } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
-
 function DeleteContent(userEmail, id, handleTrashProject) {
     const apiUrl = import.meta.env.VITE_REACT_APP_BACKEND_API;
     const [deleting, setDeleting] = useState(false)
     const navigate = useNavigate()
-
-    
     return (
         <>
-            
         </>
     )
 }
-
 export default DeleteContent
