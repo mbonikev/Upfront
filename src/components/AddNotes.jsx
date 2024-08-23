b5ca093f3c37e8e4095b0f90d87af7f4f265205f
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-23 22:03:31 +0200

    Update AddNotes.jsx

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import addnotes from '../assets/add_notes.svg'
-
 function AddNotes({ width }) {
   return (
     <img src={addnotes} className={`${width} max-w-[250px] opacity-90`} alt="Add Notes" />
 )
 }
-
 export default AddNotes
