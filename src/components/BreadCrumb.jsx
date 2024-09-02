67d16e421c04ae9a538662ca656336bb84535801
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-02 13:49:03 +0200

    Update BreadCrumb.jsx

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -4,9 +4,9 @@ function BreadCrumb({name ,link ,status}) {
   return (
     <>
         {status === 'on' ? (
-            <Link className='py-1 px-[6px] text-xs font-medium hover:bg-stone-100 rounded-md text-text-color/70 cursor-pointer' to={link}>{name}</Link>
+            <Link className='py-1 px-[6px] text-xs font-medium hover:bg-stone-100 rounded-md text-text-color/70 cursor-pointer whitespace-nowrap' to={link}>{name}</Link>
         ) : (
-            <button className='py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default'>{name}</button>
+            <button className='py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default whitespace-nowrap'>{name}</button>
         )}
     </>
   )
