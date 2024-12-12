ac3d66fcfa05033c985b73f1c6224d45ff8a31df
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-12 16:01:00 +0200

    Stalker-commit: src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -428,9 +428,7 @@ function Projects() {
                             aria-valuemax="100"
                           >
                             <div
-                              className={`flex flex-col justify-center rounded-full overflow-hidden text-xs text-white text-center whitespace-nowrap transition duration-500 ${getProgressClasses(
-                                33
-                              )}`}
+                              className={`flex flex-col justify-center rounded-full overflow-hidden text-xs text-white text-center whitespace-nowrap transition duration-500 ${getProgressClasses(33)}`}
                             ></div>
                           </div>
                           <h1 className="text-sm">100%</h1>
