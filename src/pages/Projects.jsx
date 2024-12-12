6bf1f9751ff7a7b1b0213d973ae507219390cb37
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-12 15:41:30 +0200

    Stalker-commit: src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -114,7 +114,7 @@ function Projects() {
         const response = await axios.get(`${apiUrl}/api/getmyprojects`, {
           params: { email: userEmail },
         });
-        // console.log(response.data.projects)
+        console.log(response.data.projects)
         setMyProjects(response.data.projects);
         setFetchingProjects(false);
       } catch (error) {
