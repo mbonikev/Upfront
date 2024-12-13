6b2618378dd7f9941cc71b1df30ffbb0720ecd5d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-13 12:35:40 +0200

    Stalker-commit: src/components/AddCollaborators.jsx

--- src/components/AddCollaborators.jsx
+++ src/components/AddCollaborators.jsx
@@ -80,7 +80,7 @@ function AddCollaborators({ users, username, userEmail, collaborations, id, setC
         <div className='w-full flex flex-col justify-start items-start bg-white dark:bg-[#242424]'>
             <div className='pt-2 px-2 w-full'>
                 <div className='min-h-[34px] flex items-center gap-2 px-2 py-[3px] text-sm font-normal tracking-tight line-clamp-1 '>
-                    <LuUsers2 className='text-xl text-text-color/50 min-w-fit' />
+                    <LuUsers2 className='text-xl text-text-color/50 dark:text-[#b8b8b8] min-w-fit' />
                     <div className='w-full h-fit flex-col'>
                         <p className='line-clamp-1 text-sm font-medium text-text-color'>Add Collaborators</p>
                         <p className='line-clamp-1 text-text-color/70 text-xs'>{collaborations.length} have access</p>
@@ -89,7 +89,7 @@ function AddCollaborators({ users, username, userEmail, collaborations, id, setC
             </div>
             <div className='p-2 w-full'>
                 <div className='flex items-center justify-start gap-2 relative'>
-                    <div className='text-text-color/50 pl-2'>
+                    <div className='text-text-color/50 dark:text-[#b8b8b8] pl-2'>
                         {authing ? (<RiLoader5Fill className='text-xl animate-spinLoader' />) : (<LuAtSign className='text-xl' />)}
                     </div>
                     <div className='relative'>
@@ -138,7 +138,7 @@ function AddCollaborators({ users, username, userEmail, collaborations, id, setC
             <div className='w-full h-[1px] bg-border-line-color/70'></div>
             <div className='p-2 flex flex-col w-full'>
                 <Link to={'/'} className='min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color text-sm tracking-tight rounded-md hover:bg-stone-200/50 line-clamp-1 '>
-                    <LuInfo className='text-xl text-text-color/50  min-w-fit' />
+                    <LuInfo className='text-xl text-text-color/50 dark:text-[#b8b8b8]  min-w-fit' />
                     <p className='line-clamp-1'>Access Permissions</p>
                 </Link>
             </div>
