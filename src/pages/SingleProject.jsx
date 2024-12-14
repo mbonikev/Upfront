5020bd3990836d330d7de3c698f81270ca188e1a
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-14 14:25:07 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -458,7 +458,7 @@ function SingleProject() {
       {/* Ai Modal */}
       {showAi && (
         <div
-          className={`w-[330px] h-[400px] max-h-[400px] fixed bottom-4 right-4 rounded-xl shadow-lg ring-1 ring-border-line-color/30 dark:ring-stone-600/40 overflow-y-auto z-50 bg-white dark:bg-[#242424] transition-all duration-300 flex flex-col
+          className={`w-[330px] h-[400px] max-h-[400px] fixed bottom-4 right-4 rounded-xl shadow-lg ring-1 ring-border-line-color/30 dark:ring-stone-600/40 text-dark-body dark:text-white overflow-y-auto z-50 bg-white dark:bg-[#242424] transition-all duration-300 flex flex-col
             ${
               animateShowAi
                 ? "opacity-100 translate-y-0"
