6f8451f39dddbd9781299a15690b160e5f8f4b6e
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-14 12:14:56 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -439,7 +439,9 @@ function SingleProject() {
           showAi ? "fixed cursor-default" : "hidden"
         }`}
       ></div>
-      <div className="w-[290px] h-fit max-h-[300px] p-2 absolute bottom-4 right-4 rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50 bg-white dark:bg-[#242424] "></div>
+      {showAi && (
+        <div className="w-[290px] h-fit max-h-[300px] p-2 absolute bottom-4 right-4 rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50 bg-white dark:bg-[#242424] "></div>
+      )}
 
       {/* profile menu overlay */}
       <div
