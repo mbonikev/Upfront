2c7f7528dc89a877c86de08b329e3537043e3ad7
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-14 13:03:36 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -458,14 +458,14 @@ function SingleProject() {
       {/* Ai Modal */}
       {showAi && (
         <div
-          className={`w-[350px] h-[400px] max-h-[400px] p-2 fixed bottom-4 right-4 rounded-xl shadow-lg ring-1 ring-border-line-color/30 dark:ring-stone-600/40 overflow-y-auto z-50 bg-white dark:bg-[#242424] transition-all duration-300 flex flex-col
+          className={`w-[350px] h-[400px] max-h-[400px] fixed bottom-4 right-4 rounded-xl shadow-lg ring-1 ring-border-line-color/30 dark:ring-stone-600/40 overflow-y-auto z-50 bg-white dark:bg-[#242424] transition-all duration-300 flex flex-col
             ${
               animateShowAi
                 ? "opacity-100 translate-y-0"
                 : "opacity-0 translate-y-full"
             } `}
         >
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between border-b">
             <h1 className="text-sm font-semibold px-2">Create with AI</h1>
             <button
               onClick={handleShowAi}
