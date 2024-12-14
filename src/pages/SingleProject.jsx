9ecb8c6fae6ae78061a7f65f828280a4900b87a6
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-14 11:37:25 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -421,9 +421,9 @@ function SingleProject() {
   return (
     <>
       {/* create with AI button */}
-      <button className="group fixed z-10 bottom-5 right-5 flex items-center justify-center gap-1 text-sm font-semibold px-4 py-2 rounded-xl transition hover:scale-105 bg-white text-dark-body/50 hover:text-dark-body ring-1 ring-stone-200 shadow-lg">
+      <button className="group overflow-clip w-[50px] fixed z-10 bottom-5 right-5 flex items-center justify-center gap-1 text-sm font-semibold px-4 py-2 rounded-xl transition hover:scale-105 bg-white text-dark-body/50 hover:text-dark-body ring-1 ring-stone-200 shadow-lg">
         <LuSparkles className="text-lg " />
-        <span>Create with AI</span>
+        <span className="whitespace-nowrap w-0 overflow-hidden">Create with AI</span>
       </button>
 
       {/* profile menu overlay */}
