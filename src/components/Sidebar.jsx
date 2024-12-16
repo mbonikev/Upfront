696d826e14679ec16ec24c24ba95c606a0e3758d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-16 21:29:12 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -181,7 +181,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
         )}
         <div className="w-full h-fit min-h-svh max-h-svh border-r-[1px] border-border-line-color/20 dark:border-[#313131a6] bg-sidebar-color dark:bg-[#202020] dark:text-[#b8b8b8] flex flex-col gap-[2px] p-2 text-sm overflow-y-auto">
           {/* 1 */}
-          <div className="w-full h-fit flex items-center justify-between mb-4">
+          <div className="w-full h-fit mb-4">
             <button
               onClick={showPMenu}
               className="dark:text-[#b8b8b8] text-text-color w-full flex items-center justify-start gap-[2px] hover:bg-stone-200 dark:hover:bg-[#2c2c2c] transition p-1 rounded-lg"
