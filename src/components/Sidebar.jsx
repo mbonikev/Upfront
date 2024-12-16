ecb537d4c628e41a6de9dc460810bc6c08575bf4
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-16 20:10:40 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -233,8 +233,8 @@ function Sidebar({
                         to={"/"}
                         className={`${linkStyle} hover:bg-stone-200/50 group-hover:bg-stone-200/50`}
                     >
-                        <div className="text-text-color/50 dark:text-[#858585] text-xl w-[20px] min-w-[20px] h-[20px] bg-red-500 relative overflow-clip" >
-                            <div className="border-[2px] h-full w-full"></div>
+                        <div className="text-text-color/50 dark:text-[#858585] text-xl w-[20px] min-w-[20px] h-[20px] relative overflow-clip" >
+                            <div className="border-[2px] h-full w-full border-text-color/50 dark:border-[#858585] "></div>
                         </div>
                         <p className="line-clamp-1">Today</p>
                     </Link>
