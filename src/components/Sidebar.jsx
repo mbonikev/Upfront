0a735b30606077a6bd75a648a446cbebc3c43d96
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-16 20:46:11 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -248,8 +248,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
             className={`${linkStyle} hover:bg-stone-200/50 group-hover:bg-stone-200/50`}
           >
             <div className="text-text-color/50 dark:text-[#858585] text-xl bg-red-200/0 w-[20px] min-w-[20px] h-[20px] relative pt-[0px] flex flex-col items-center justify-center gap-[2px]">
-              <div className="w-[17px] h-[2px] min-h-[2px] rounded-[5px] bg-text-color/50 dark:bg-[#858585] z-10 relative"></div>
-              <LuMinus />
+              <LuMinus className="text-text-color/50 dark:text-[#858585] text-[15px]" />
               <div className="w-fit h-fit text-text-color/50 dark:text-[#858585] text-[15px] font-semibold leading-none z-10 tracking-wide">
                 {Today}
               </div>
