d01ff208140f98076764053ac0245f28cec88ff1
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-16 21:16:42 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -256,6 +256,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
                 </div>
               </div>
               <p className="line-clamp-1">Today</p>
+              <p className="line-clamp-1">Today</p>
             </Link>
             <p className="flex items-center justify-between gap-2 pt-[13px] pb-[7px] px-[10px] font-medium dark:text-[#f1f1f1]/70 text-text-color/70 tracking-tight">
               <span>Workspaces</span>
