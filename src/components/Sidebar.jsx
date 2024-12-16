31af4764d1d1d630270a57b420e2afbc414fa924
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-16 20:39:11 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -243,12 +243,12 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
             to={"/"}
             className={`${linkStyle} hover:bg-stone-200/50 group-hover:bg-stone-200/50`}
           >
-            <div className="text-text-color/50 dark:text-[#858585] text-xl bg-red-200/0 w-[20px] min-w-[20px] h-[20px] relative pt-[0px] flex flex-col items-center justify-center gap-[0px]">
+            <div className="text-text-color/50 dark:text-[#858585] text-xl bg-red-200/0 w-[20px] min-w-[20px] h-[20px] relative pt-[0px] flex flex-col items-center justify-center gap-[2px]">
               <div className="w-[16px] h-[2px] min-h-[2px] rounded-[5px] bg-text-color/50 dark:bg-[#858585] z-10 relative">
                 <div className="h-[4px] w-[2px] rounded-[5px] bg-text-color/50 dark:bg-[#858585] absolute top-[-2px] left-[2px]"></div>
                 <div className="h-[4px] w-[2px] rounded-[5px] bg-text-color/50 dark:bg-[#858585] absolute top-[-2px] right-[2px]"></div>
               </div>
-              <div className="w-fit h-fit bg-red-400 flex-1 text-text-color/50 dark:text-[#858585] text-[14px] font-semibold leading-none z-10 tracking-wide">
+              <div className="w-fit h-fit text-text-color/50 dark:text-[#858585] text-[14px] font-semibold leading-none z-10 tracking-wide">
                 16
               </div>
             </div>
