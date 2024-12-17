759cadb3b74a25fdd0a287e20ae6af0816517f7c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-17 08:47:36 +0200

    Stalker-commit: src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -328,7 +328,7 @@ function Projects() {
               ) : (
                 <div className="grid grid-cols-3 2xl:grid-cols-5 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-3 pt-4 relative">
                   <div
-                    className="h-full relative w-full rounded-2xl bg-transparent border-[3px] border-dashed flex flex-col gap-4 p-4"
+                    className="h-full relative w-full rounded-2xl bg-transparent border-[3px] dark:border-[#303030] transition border-dashed flex flex-col gap-4 p-4"
                   ></div>
                   {myProjects.map((project, index) => (
                     <div
