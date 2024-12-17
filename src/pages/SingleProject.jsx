129629ac93ebd64a759b382dbe5fa2fd809297a6
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-17 07:30:34 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -669,7 +669,7 @@ function SingleProject() {
         )}
         {/* Collab dropdown */}
         {userMenu && (
-          <div className="w-[290px] h-fit max-h-[80vh] absolute top-[52px] right-[190px] rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50">
+          <div className="w-[290px] h-fit max-h-[80vh] absolute top-[52px] right-[180px] rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50">
             <AddCollaborators
               users={users}
               username={username}
@@ -682,6 +682,45 @@ function SingleProject() {
         )}
         {/* Delete Dropdown */}
         {deleteMenu && (
+          <div className="w-[290px] h-fit max-h-[80vh] p-2 absolute top-[52px] right-[210px] rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50 bg-white dark:bg-[#242424] ">
+            <p className="text-sm text-text-color/70 dark:text-[#b8b8b8] px-2 pt-2 pb-4">
+              <span className="font-medium text-text-color dark:text-white">
+                Warning!{" "}
+              </span>{" "}
+              Deleting this project will remove it from your workspace and move
+              it to trash. Collaborations will be stashed for possible future
+              restoration
+            </p>
+            <p className="text-sm text-text-color/70 dark:text-[#b8b8b8] px-2 pb-4">
+              {" "}
+              Some fields, like{" "}
+              <span className="font-medium text-text-color dark:text-white">
+                'createdAt'
+              </span>{" "}
+              will reset during restore.
+            </p>
+            <div className="flex items-center justify-end">
+              <button
+                onClick={handleTrashProject}
+                title="Trash"
+                className="bg-red-500 text-white transition text-xs font-semibold h-[35px] py-0 px-3 w-full gap-1 shadow-md rounded-lg inline-flex items-center justify-center"
+              >
+                {deleting ? (
+                  <RiLoader5Fill className="text-2xl animate-spinLoader" />
+                ) : (
+                  <>
+                    <LuTrash2 className="text-lg" />
+                    <span className="text-sm font-medium tracking-tight">
+                      Move to Trash
+                    </span>
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
+        )}
+        {/* Wiper Dropdown */}
+        {wipeMenu && (
           <div className="w-[290px] h-fit max-h-[80vh] p-2 absolute top-[52px] right-[210px] rounded-xl shadow-custom ring-1 ring-border-line-color/0 overflow-y-auto z-50 bg-white dark:bg-[#242424] ">
             <p className="text-sm text-text-color/70 dark:text-[#b8b8b8] px-2 pt-2 pb-4">
               <span className="font-medium text-text-color dark:text-white">
