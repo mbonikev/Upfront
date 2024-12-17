bced998d9ce0b094f3ea7f0e22038f066ba46b43
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-17 08:02:53 +0200

    Stalker-commit: src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -369,6 +369,7 @@ function SingleProject() {
     try {
       const response = await axios.post(`${apiUrl}/api/wipe`, {
         projectId: id,
+        userEmail
         choice
       });
       setWipeMenu(false)
