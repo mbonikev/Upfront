9bfc9da2a8624fd247c3a8541edae8c39dccdf5d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 18:33:29 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -8,7 +8,7 @@ function SearchModal() {
         <div>
           <LuSearch className="text-2xl" />
         </div>
-        <input type="text" className="bg-transparent w-full text-lg" placeholder="Project name, Task name, Template name or Keywords..." />
+        <input type="text" className="bg-transparent w-full text-lg text-white placeholder:text-[#9b9b9b]" placeholder="Project name, Task name, Template name or Keywords..." />
       </div>
     </div>
   );
