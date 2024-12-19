2486a9fc1e4246e718924d06c9dc0c8edf00beca
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 19:48:01 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -33,8 +33,8 @@ function SearchModal() {
             <span className="text-[#f0f0f0] tracking-tight">Templates</span>
           </button>
         </div>
-        <div className="w-2/3 h-full flex flex-col">
-            <div className="flex items-center justify-between pl-3 pr-3 h-[40px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
+        <div className="w-2/3 h-full flex flex-col pr-4">
+            <div className="flex items-center justify-between cursor-pointer pl-3 pr-3 h-[40px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
                 <span>The Green Project</span>
                 <span>21</span>
             </div>
