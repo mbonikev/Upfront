7bd05d3d55527c67cba530dd17b7efdeba7bfca2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 19:20:30 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -9,14 +9,23 @@ function SearchModal() {
         <div>
           <LuSearch className="text-xl text-white" />
         </div>
-        <input type="text" autoFocus={true} className="tracking-tight bg-transparent w-full text-lg text-white placeholder:text-[#a1a1a1]" placeholder="Project name, Task name, Template name or Keywords..." />
+        <input
+          type="text"
+          autoFocus={true}
+          className="tracking-tight bg-transparent w-full text-lg text-white placeholder:text-[#a1a1a1]"
+          placeholder="Project name, Task name, Template name or Keywords..."
+        />
       </div>
       <div className="w-full flex-1 mt-5 flex items-start justify-start gap-5">
-        <div className="w-1/3 h-full flex items-start justify-start">
-            <button className="outline-none flex items-center justify-start bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
-                <IoFolderOpen className="text-xl" />
-                <span className="text-white tracking-tight">Projects</span>
-            </button>
+        <div className="w-1/3 h-full flex items-start justify-start flex-col gap-0.5">
+          <button className="outline-none flex items-center justify-start bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+            <IoFolderOpen className="text-xl" />
+            <span className="text-white tracking-tight">Projects</span>
+          </button>
+          <button className="outline-none flex items-center justify-start bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+            <IoFolderOpen className="text-xl" />
+            <span className="text-white tracking-tight">Projects</span>
+          </button>
         </div>
         <div className="w-2/3 h-full bg-green-500"></div>
       </div>
