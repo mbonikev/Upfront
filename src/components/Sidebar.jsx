4aae1204d0afff78469d26f1ce245b318de76e16
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 19:38:31 +0200

    Stalker-commit: src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -187,7 +187,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
           }`}
         ></div>
         {/* search modal */}
-        <div className="w-fit h-full max-h-[63%] bg-[#000000bd] backdrop-blur-[20px] rounded-[25px] fixed top-0 left-0 right-0 bottom-0 m-auto shadow-custom ring-1 ring-border-line-color/0 z-30">
+        <div className="w-fit h-full max-h-[63%] bg-[#000000c3] backdrop-blur-[30px] rounded-[25px] fixed top-0 left-0 right-0 bottom-0 m-auto shadow-custom ring-1 ring-border-line-color/0 z-30">
           <SearchModal />
         </div>
         {/* dropdown */}
