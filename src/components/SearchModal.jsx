b1aa7acea851701a74ce3189df819be2046a6798
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 18:36:59 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -4,9 +4,9 @@ import { LuSearch } from "react-icons/lu";
 function SearchModal() {
   return (
     <div className="w-[810px] h-[409px] p-4 flex flex-col">
-      <div className="flex items-center justify-start gap-3 h-[45px] px-3 text-[#a1a1a1]">
+      <div className="flex items-center justify-start gap-3 h-[35px] px-1 text-[#a1a1a1]">
         <div>
-          <LuSearch className="text-2xl" />
+          <LuSearch className="text-xl text-white" />
         </div>
         <input type="text" autoFocus={true} className="tracking-tight bg-transparent w-full text-lg text-white placeholder:text-[#a1a1a1]" placeholder="Project name, Task name, Template name or Keywords..." />
       </div>
