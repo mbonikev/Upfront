06e63169471573b7295a4ec4d8a9d5a2b5ed8828
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 18:27:29 +0200

    Stalker-commit: src/components/SearchModal.jsx src/components/Sidebar.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchModal() {
   return (
-    <div className='w-[810px] h-[409px] '>SearchModal</div>
+    <div className='w-[810px] h-[409px] p-4 '>SearchModal</div>
   )
 }
 
