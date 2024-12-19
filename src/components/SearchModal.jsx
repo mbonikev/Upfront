d5e8006404a41f90bd1bc3a566182cfa9f0b2cb0
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 19:33:31 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FiCheckCircle } from "react-icons/fi";
 import { IoFolderOpen } from "react-icons/io5";
 import { LuSearch } from "react-icons/lu";
-import { PiPersonSimpleRunBold } from "react-icons/pi";
+import { PiPersonSimpleRunBold, PiProjectorScreenChart } from "react-icons/pi";
 
 function SearchModal() {
   return (
     <div className="w-[810px] h-full px-5 py-[15px] flex flex-col">
       <div className="flex items-center justify-start gap-3 h-[35px] px-1 text-[#a1a1a1]">
         <div>
-          <LuSearch className="text-xl text-white" />
+          <LuSearch className="text-xl text-[#f0f0f0]" />
         </div>
         <input
           type="text"
@@ -20,8 +20,8 @@ function SearchModal() {
       </div>
       <div className="w-full flex-1 mt-6 flex items-start justify-start gap-5">
         <div className="w-1/3 h-full flex items-start justify-start flex-col gap-0.5">
-          <button className="outline-none flex items-center justify-start bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-red-300 text-[16px] font-medium ">
-            <IoFolderOpen className="text-xl" />
+          <button className="outline-none flex items-center justify-start bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+            <PiProjectorScreenChart className="text-xl" />
             <span className="text-[#f0f0f0] tracking-tight">Projects</span>
           </button>
           <button className="outline-none flex items-center justify-start hover:bg-[#4b4b4b] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
