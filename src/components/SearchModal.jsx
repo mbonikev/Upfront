86d8eb2f845829f971001a7008dc91dcbf91f524
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 18:31:59 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { LuSearch } from 'react-icons/lu'
+import React from "react";
+import { LuSearch } from "react-icons/lu";
 
 function SearchModal() {
   return (
-    <div className='w-[810px] h-[409px] p-4 flex flex-col'>
-        <div className='flex items-center justify-start bg-orange-500 h-[45px] px-3'>
-            <div><LuSearch className='text-2xl' /></div>
+    <div className="w-[810px] h-[409px] p-4 flex flex-col">
+      <div className="flex items-center justify-start gap-3 bg-orange-500 h-[45px] px-3">
+        <div>
+          <LuSearch className="text-2xl" />
         </div>
+        <input type="text" className="bg-transparent" placeholder="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchModal
+export default SearchModal;
