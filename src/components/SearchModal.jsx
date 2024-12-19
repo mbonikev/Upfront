0f7fec3a05e0d915fa64dea393bc835665ed7cfb
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 18:59:29 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -4,7 +4,7 @@ import { LuSearch } from "react-icons/lu";
 function SearchModal() {
   return (
     <div className="w-[810px] h-[409px] p-4 flex flex-col">
-      <div className="flex items-center justify-start gap-3 h-[35px] px-1 text-[#a1a1a1]">
+      <div className="flex items-center justify-start gap-3 h-[35px] px-1 text-[#a1a1a1] bg-red-400">
         <div>
           <LuSearch className="text-xl text-white" />
         </div>
