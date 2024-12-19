5169592f3e7d22109f95f249659d7a89fe2e1e37
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-19 19:06:30 +0200

    Stalker-commit: src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -11,7 +11,8 @@ function SearchModal() {
         <input type="text" autoFocus={true} className="tracking-tight bg-transparent w-full text-lg text-white placeholder:text-[#a1a1a1]" placeholder="Project name, Task name, Template name or Keywords..." />
       </div>
       <div className="w-full flex-1 mt-5 flex items-start justify-start gap-5">
-        <div className="w-1/3 h-full bg-green-500"></div>
+        <div className="w-1/3 h-full flex">
+        </div>
         <div className="w-2/3 h-full bg-green-500"></div>
       </div>
     </div>
