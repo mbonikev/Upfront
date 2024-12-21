c4340de0083c554334c1af458393f09c68545473
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 21:20:43 +0200

    updated src/components/ProfileDropdownButtons.jsx

--- src/components/ProfileDropdownButtons.jsx
+++ src/components/ProfileDropdownButtons.jsx
@@ -33,7 +33,7 @@ function ProfileDropdownButtons({ username, userEmail }) {
   return (
     <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]">
       <div className="p-2 w-full">
-        <div className="w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 ">
+        <div className="w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal">
           <p className="h-[25px] w-auto aspect-square rounded-full bg-main-color dark:bg-[#424242] text-white transition flex items-center justify-center text-sm font-semibold uppercase">
             {username.charAt(0)}
           </p>
