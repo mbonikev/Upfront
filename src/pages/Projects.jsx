9d482ca7e6a05203aad04701cc81acd6850f53c9
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 19:59:10 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -463,8 +463,8 @@ function Projects() {
                           <h1 className="text-sm">100%</h1>
                         </div>
                         <div className="w-full flex items-start justify-start flex-col gap-2">
-                          <p className="w-fit flex items-center justify-start text-xs gap-1 font-medium text-text-color/70 dark:text-[#b8b8b8]">
-                            {/* <TbStack className="text-xl" /> */}
+                          <p className="w-fit flex items-center justify-start text-xs gap-1 font-medium text-text-color/70 dark:text-[#b8b8b8]/70">
+                            <TbStack className="text-xl" />
                             {
                               projectBoards.filter(
                                 (board) => board.belongsTo === project._id
@@ -479,7 +479,7 @@ function Projects() {
                               ? "Boards"
                               : "Board"}
                           </p>
-                          <p className="w-fit flex items-start justify-end text-xs font-medium px-[2px] text-text-color/70 dark:text-[#b8b8b8]">
+                          <p className="w-fit flex items-start justify-end text-xs font-medium px-[2px] text-text-color/70 dark:text-[#b8b8b8]/70">
                             {project.progress === 100
                               ? "Completed"
                               : format(
