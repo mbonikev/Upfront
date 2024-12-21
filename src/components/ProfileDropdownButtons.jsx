e7c04cfcaa34727227ba27887b0b78340d3a0405
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 21:09:43 +0200

    updated src/components/ProfileDropdownButtons.jsx

--- src/components/ProfileDropdownButtons.jsx
+++ src/components/ProfileDropdownButtons.jsx
@@ -1,61 +1,103 @@
-import React, { useState } from 'react'
-import { LuActivity, LuArchive, LuAtSign, LuInfo, LuLogOut, LuSettings, LuTrash2, LuTrophy } from 'react-icons/lu'
-import { RiLoader5Fill } from 'react-icons/ri'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
+import {
+  LuActivity,
+  LuArchive,
+  LuAtSign,
+  LuInfo,
+  LuLogOut,
+  LuSettings,
+  LuTrash2,
+  LuTrophy,
+} from "react-icons/lu";
+import { RiLoader5Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 function ProfileDropdownButtons({ username }) {
-    const [logoutAnimate, setLogoutAnimate] = useState(false)
-    const handleLogout = () => {
-        localStorage.removeItem('upfront_user')
-        localStorage.removeItem('upfront_user_name')
-        localStorage.removeItem('upfront_user_name_w1')
-        localStorage.removeItem('mycollaborations')
-        setLogoutAnimate(true)
-        setTimeout(() => {
-            window.location.reload()
-        }, 1000);
-    }
-    return (
-        <div className='w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]'>
-            <div className='p-2 w-full'>
-                <button to={'/'} className='w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 '>
-                    <LuTrophy className='text-xl text-text-color/50 dark:text-[#b8b8b8]/50 min-w-fit' />
-                    <div className='w-full h-fit flex flex-col justify-center items-start'>
-                        <p className='line-clamp-1 text-sm font-medium text-text-color dark:text-[#b8b8b8]'>{username}</p>
-                        <p className='line-clamp-1 text-text-color/70 text-xs dark:text-[#b8b8b8]/70'>242 Completed </p>
-                    </div>
-                </button>
-            </div>
-            <div className='w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]'></div>
-            <div className='p-2 flex flex-col w-full'>
-                <Link to={'/'} className='min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 '>
-                    <LuSettings className='text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit' />
-                    <p className='line-clamp-1'>Settings</p>
-                </Link>
-                <Link to={'/'} className='min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 '>
-                    <LuTrash2 className='text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit' />
-                    <p className='line-clamp-1'>Trash</p>
-                </Link>
-            </div>
-            <div className='w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]'></div>
-            <div className='p-2 flex flex-col w-full'>
-                <Link to={'/'} className='min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 '>
-                    <LuInfo className='text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit' />
-                    <p className='line-clamp-1'>About Upfront</p>
-                </Link>
-            </div>
-            <div className='w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]'></div>
-            <div className='p-2 flex flex-col w-full'>
-                <button onClick={handleLogout} className={`min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 ${logoutAnimate ? 'pointer-events-none' : ''}`}>
-                    {logoutAnimate ? <>
-                        <RiLoader5Fill className='text-xl animate-spinLoader text-red-500  min-w-fit' />
-                        <p className='line-clamp-1'>Logging out..</p>
-                    </> : <>
-                        <LuLogOut className='text-xl text-red-500  min-w-fit' />
-                        <p className='line-clamp-1'>Log out</p>
-                    </>}
-                </button>
-            </div>
-        </div>
-    )
+  const [logoutAnimate, setLogoutAnimate] = useState(false);
+  const handleLogout = () => {
+    localStorage.removeItem("upfront_user");
+    localStorage.removeItem("upfront_user_name");
+    localStorage.removeItem("upfront_user_name_w1");
+    localStorage.removeItem("mycollaborations");
+    setLogoutAnimate(true);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  };
+  return (
+    <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]">
+      <div className="p-2 w-full">
+        <button
+          to={"/"}
+          className="w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 "
+        >
+          <LuTrophy className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50 min-w-fit" />
+          <div className="w-full h-fit flex flex-col justify-center items-start">
+            <p className="line-clamp-1 text-sm font-medium text-text-color dark:text-[#b8b8b8]">
+              {username}
+            </p>
+            <p className="line-clamp-1 text-text-color/70 text-xs dark:text-[#b8b8b8]/70">
+              242 Completed{" "}
+            </p>
+          </div>
+        </button>
+      </div>
+      <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
+      <div className="p-2 flex flex-col w-full">
+        <Link
+          to={"/"}
+          className="min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 "
+        >
+          <LuSettings className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <p className="line-clamp-1">Settings</p>
+        </Link>
+        <Link
+          to={"/"}
+          className="min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 "
+        >
+          <LuTrash2 className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <p className="line-clamp-1">Trash</p>
+        </Link>
+      </div>
+      <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
+      <div className="p-2 flex flex-col w-full">
+        <Link
+          to={"/"}
+          className="min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 "
+        >
+          <LuInfo className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <p className="line-clamp-1">About Upfront</p>
+        </Link>
+        <Link
+          to={"/"}
+          className="min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 "
+        >
+          <HiOutlineSpeakerphone className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <p className="line-clamp-1">Updates</p>
+        </Link>
+      </div>
+      <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
+      <div className="p-2 flex flex-col w-full">
+        <button
+          onClick={handleLogout}
+          className={`min-h-[34px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 ${
+            logoutAnimate ? "pointer-events-none" : ""
+          }`}
+        >
+          {logoutAnimate ? (
+            <>
+              <RiLoader5Fill className="text-xl animate-spinLoader text-red-500  min-w-fit" />
+              <p className="line-clamp-1">Logging out..</p>
+            </>
+          ) : (
+            <>
+              <LuLogOut className="text-xl text-red-500  min-w-fit" />
+              <p className="line-clamp-1">Log out</p>
+            </>
+          )}
+        </button>
+      </div>
+    </div>
+  );
 }
-export default ProfileDropdownButtons
+export default ProfileDropdownButtons;
