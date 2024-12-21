bafb32e90737347658183775889d4f90d3bc458c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 21:48:44 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -371,7 +371,7 @@ function Projects() {
                     {!createNew ? (
                       <div className="flex items-center gap-1 text-main-color">
                         <RiLoader5Fill className="text-2xl animate-spinLoader" />
-                        <p className="line-clamp-2 max-w-[200px]">
+                        <p className="line-clamp-2 max-w-[190px]">
                           Setting up your project...{" "}
                         </p>
                       </div>
