72fa16bbe743381c04ac127dde77928ef4d639ac
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 13:35:14 +0200

    updated src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FiCheckCircle } from "react-icons/fi";
 import { IoFolderOpen } from "react-icons/io5";
 import { LuFiles, LuHash, LuSearch } from "react-icons/lu";
 import { PiPersonSimpleRunBold, PiProjectorScreenChart } from "react-icons/pi";
 
-function SearchModal() {
+function SearchModal({ Hide }) {
+  useEffect(() => {
+    const handleKeyDown = (event) => event.key === "Escape" && Hide();
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [Hide]);
   return (
     <div className="w-[810px] h-full pl-5 pr-1 py-[15px] flex flex-col">
       <div className="flex items-center justify-start gap-3 h-[35px] px-1 text-[#a1a1a1]">
@@ -20,27 +25,29 @@ function SearchModal() {
       </div>
       <div className="w-full flex-1 mt-6 flex items-start justify-start gap-5">
         <div className="w-1/3 h-full flex items-start justify-start flex-col gap-0.5">
-          <button className="outline-none flex items-center justify-start bg-[#505050] dark:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-semibold ">
+          <button className="outline-none flex items-center justify-start bg-[#505050] dark:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
             <LuHash className="text-xl" />
             <span className="text-[#f0f0f0] tracking-tight">Projects</span>
           </button>
-          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-semibold ">
+          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
             <FiCheckCircle className="text-xl" />
             <span className="text-[#f0f0f0] tracking-tight">Tasks</span>
           </button>
-          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-semibold ">
+          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
             <PiPersonSimpleRunBold className="text-xl" />
             <span className="text-[#f0f0f0] tracking-tight">Templates</span>
           </button>
         </div>
         <div className="w-2/3 h-full flex flex-col pr-4">
-          <span className=" font-medium text-sm px-3 text-[#989898] mb-2">Project Names</span>
-          <div className="text-[#f0f0f0] flex items-center text-base justify-between cursor-pointer pl-[12px] pr-3 h-[42px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
-            <span className=" font-semibold">The Green Project</span>
+          <span className=" font-medium text-sm px-3 text-[#989898] mb-2">
+            Project Names
+          </span>
+          <div className="text-[#f0f0f0] flex items-center text-base justify-between cursor-pointer pl-[12px] pr-3 h-[40px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
+            <span className=" font-normal text-sm">The Green Project</span>
             <span className="opacity-55 text-base">21</span>
           </div>
-          <div className="text-[#f0f0f0] flex items-center text-base justify-between cursor-pointer pl-[12px] pr-3 h-[42px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
-            <span className=" font-semibold">The Green Project</span>
+          <div className="text-[#f0f0f0] flex items-center text-base justify-between cursor-pointer pl-[12px] pr-3 h-[40px] rounded-xl hover:bg-[#505050] dark:hover:bg-[#2f2f2f]">
+            <span className=" font-normal text-sm">The Green Project</span>
             <span className="opacity-55 text-base">21</span>
           </div>
         </div>
