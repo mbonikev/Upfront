e4810b5a4010e4521757fd5d95b0822560fa8458
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 09:33:25 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -312,12 +312,10 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
               {createNew ? (
                 <div className="flex items-center gap-2">
                   <RiLoader5Fill className="text-2xl animate-spinLoader" />
-                  <p className="line-clamp-1 ">Setting up your project... </p>
                 </div>
               ) : (
                 <div className="flex items-center gap-2">
                   <IoIosAddCircle className="text-2xl" />
-                  <p className="line-clamp-1 ">New Project</p>
                 </div>
               )}
             </button>
