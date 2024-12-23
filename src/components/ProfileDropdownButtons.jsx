a78d1c5890c4b721b9f8ac5a0635f7f09fa1a5a2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 09:56:56 +0200

    updated src/components/ProfileDropdownButtons.jsx

--- src/components/ProfileDropdownButtons.jsx
+++ src/components/ProfileDropdownButtons.jsx
@@ -25,10 +25,10 @@ function ProfileDropdownButtons({ username, userEmail }) {
     }, 1000);
   };
 
-  const linkStyle = `min-h-[32px] flex items-center gap-2 px-2 py-[7px] font-normal text-text-color dark:text-[#b8b8b8] text-sm tracking-tight rounded-md hover:bg-stone-200/50 dark:hover:bg-[#383838] line-clamp-1 `
+  const linkStyle = `h-[32px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] text-text-color/90 tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#323232]`;
 
   return (
-    <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]">
+    <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#262626]">
       <div className="p-2 w-full cursor-default">
         <div className="w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal">
           <p className="h-[35px] w-auto aspect-square rounded-full bg-main-color dark:bg-[#424242] text-white transition flex items-center justify-center text-lg font-bold uppercase">
@@ -46,34 +46,22 @@ function ProfileDropdownButtons({ username, userEmail }) {
       </div>
       <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
       <div className="p-2 flex flex-col w-full">
-        <Link
-          to={"/"}
-          className={`${linkStyle}`}
-        >
+        <Link to={"/"} className={`${linkStyle}`}>
           <LuSettings className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
           <p className="line-clamp-1">Settings</p>
         </Link>
-        <Link
-          to={"/"}
-          className={`${linkStyle}`}
-        >
+        <Link to={"/"} className={`${linkStyle}`}>
           <LuTrash2 className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
           <p className="line-clamp-1">Trash</p>
         </Link>
       </div>
       <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
       <div className="p-2 flex flex-col w-full">
-        <Link
-          to={"/"}
-          className={`${linkStyle}`}
-        >
+        <Link to={"/"} className={`${linkStyle}`}>
           <LuInfo className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
           <p className="line-clamp-1">About Upfront</p>
         </Link>
-        <Link
-          to={"/"}
-          className={`${linkStyle}`}
-        >
+        <Link to={"/"} className={`${linkStyle}`}>
           <HiOutlineSpeakerphone className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
           <p className="line-clamp-1">Updates</p>
         </Link>
