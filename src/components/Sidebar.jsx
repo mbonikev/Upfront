345e62de92eed395f899dbaf0fea784d2d54e4bd
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 08:54:23 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,7 +42,6 @@ import { HiOutlineSpeakerphone } from "react-icons/hi";
 import SearchModal from "./SearchModal";
 import Reveal, { Fade } from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
-import { CgRename } from "react-icons/cg";
 const customAnimation = keyframes`
   from {
     opacity: 0;
@@ -447,7 +446,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
                       onClick={renameW1}
                       className={`${linkStyle} hover:bg-stone-100 dark:hover:bg-[#383838]`}
                     >
-                      <CgRename className="text-base min-w-fit" />
+                      <LuPencil className="text-base min-w-fit" />
                       <p className="line-clamp-1">Rename</p>
                     </button>
                     <button
