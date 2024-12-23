7285866d06f86d2ab111a116d6cb074c55fb0b7a
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 10:26:27 +0200

    updated src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -125,8 +125,7 @@ function SingleProject() {
     e.preventDefault();
     setGenerating(true);
     try {
-      const response = await axios.post(`${apiUrl}`
-        "http://localhost:5000/api/generateBoards",
+      const response = await axios.post(`${apiUrl}/api/generateBoards`,
         {
           projectDescription,
           userEmail,
