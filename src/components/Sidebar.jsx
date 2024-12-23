1cde63f756fe5f8aa5f5f310ff257ba3f37633c5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 09:03:23 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -179,7 +179,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
   // get collabs
   const retrieveArray = getArray("mycollaborations") ?? [];
   const linkStyle =
-    "h-[28px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] text-text-color/90 tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#323232]";
+    "h-[29px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] text-text-color/90 tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#323232]";
 
   const getDate = new Date();
   const Today = getDate.getDate();
