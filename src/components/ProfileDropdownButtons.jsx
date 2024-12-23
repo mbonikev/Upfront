a885abe34e4eaf85d263c41d19af180ee8b28396
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 09:58:56 +0200

    updated src/components/ProfileDropdownButtons.jsx

--- src/components/ProfileDropdownButtons.jsx
+++ src/components/ProfileDropdownButtons.jsx
@@ -25,25 +25,10 @@ function ProfileDropdownButtons({ username, userEmail }) {
     }, 1000);
   };
 
-  const linkStyle = `h-[32px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] dark:hover:bg-[#383838] text-text-color/90 tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#404040]`;
+  const linkStyle = `h-[32px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] dark:hover:bg-[#383838] text-sm text-text-color/90 tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#404040]`;
 
   return (
     <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]">
-      <div className="p-2 w-full cursor-default">
-        <div className="w-full min-h-[34px] flex items-center justify-start gap-2 px-2 py-[3px] text-sm font-normal">
-          <p className="h-[35px] w-auto aspect-square rounded-full bg-main-color dark:bg-[#424242] text-white transition flex items-center justify-center text-lg font-bold uppercase">
-            {username.charAt(0)}
-          </p>
-          <div className="w-full h-fit flex flex-col justify-center items-start">
-            <p className="line-clamp-1 text-sm tracking-tight  font-medium text-text-color dark:text-[#b8b8b8]">
-              {username}
-            </p>
-            <p className="line-clamp-1 text-text-color/70 text-xs dark:text-[#b8b8b8]/70">
-              {userEmail}
-            </p>
-          </div>
-        </div>
-      </div>
       <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
       <div className="p-2 flex flex-col w-full">
         <Link to={"/"} className={`${linkStyle}`}>
