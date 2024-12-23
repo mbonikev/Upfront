14f38493b9b5b9d8718868b4f95e69640d1a1cf8
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-23 08:37:17 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -428,7 +428,7 @@ function Sidebar({ handleSidebarToggle, username, userEmail, w1, setW1 }) {
                   style={{ top: menuPosition.top, left: menuPosition.left }}
                   className="fixed bg-white dark:bg-[#2c2c2c] dark:shadow-custom2 rounded-xl w-fit min-w-[160px] h-fit shadow-md z-[1000] ring-1 ring-border-line-color/50 dark:ring-stone-600/30 p-1 transition-all ease-in-out duration-300 overflow-clip"
                 >
-                  <Reveal keyframes={customAnimation} duration={50} triggerOnce damping={0.1} cascade={true}>
+                  <Reveal keyframes={customAnimation} duration={230} triggerOnce damping={0.1} cascade={true}>
                     <div
                       onClick={renameW1}
                       className={`${linkStyle} cursor-pointer hover:bg-stone-100 dark:hover:bg-[#383838]`}
