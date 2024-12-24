ebc9e8fba510a9d285e3e52dced44d1a05633cc9
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-24 09:07:46 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -38,7 +38,7 @@ function Projects() {
         const response = await axios.get(`${apiUrl}/api/workspaces`, {
           params: { userEmail },
         });
-        console.log('Response data:', response);
+        // console.log('Response data:', response);
         localStorage.setItem("upfront_ws", JSON.stringify(response.data.workspaces));
       } catch (err) {
         console.error("Error updating data:", err);
