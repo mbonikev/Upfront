05f1352c61b1dda401741ecca8f8699f03781ad0
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 09:31:07 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -145,7 +145,7 @@ function Projects() {
       <div
         className={`w-full h-full min-h-svh flex-1 text-text-color bg-stone-50 dark:bg-dark-body transition-all duration-500 ease-in-out z-10 overflow-clip flex items-start justify-start p-1`}
       >
-        <div className="relative w-auto flex-1 bg-red-200 min-h-[50px] max-w-[2300px] mx-auto px-7 max-xl:px-0 flex items-start justify-start">
+        <div className="relative w-full flex-1 bg-red-200 min-h-[50px] max-w-[2300px] mx-auto px-7 max-xl:px-0 flex items-start justify-start">
           {/* Left Button */}
           {canScrollLeft && (
             <div className="nextSpace w-[80px] h-full absolute top-0 left-5 max-xl:left-0 bg-gradient-to-r max-xl:pl-2 from-white via-white to-transparent z-20 flex items-center justify-start">
