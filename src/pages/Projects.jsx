4d47b48b529ceffd57d187709f93a19eb86e6007
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 15:23:09 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -294,7 +294,7 @@ function Projects() {
           </div>
         </div>
         <div className="w-full h-fit flex items-start justify-between px-5 py-5 max-w-[2000px]">
-          {fetchingProjects ? (
+          {!fetchingProjects ? (
             <>
               <div className="group z-10 relative w-[200px] h-[20px] animate-smooth-pulse rounded-xl dark:bg-[#313131] bg-stone-200"></div>
             </>
