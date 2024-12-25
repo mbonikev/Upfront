0da23eabc6aced6c9ec92f682645becbf23eda57
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 11:57:39 +0200

    updated src/components/Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="fixed top-0 z-50 left-0 w-full h-full bg-white dark:bg-dark-body flex items-center justify-center flex-col gap-3">
+      <img
+        src={}
+        loading="lazy"
+        className="h-16 saturate-100 aspect-square"
+      />
+      <span class="loader"></span>
+    </div>
+  );
+}
+
+export default Loader;
