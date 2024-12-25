bedf56faa768db6c6e6cd16ff064417bf0772aa2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 15:22:09 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -315,10 +315,10 @@ function Projects() {
         {/* Projects section */}
         <div className="w-full h-full flex-1 px-6 pb-10 max-w-[2000px] flex flex-col">
           <div className="w-full h-fit flex items-end justify-between">
-            {fetchingProjects ? (
+            {!fetchingProjects ? (
               <>
-                <div className="group z-10 relative w-2/6 h-[10px] animate-smooth-pulse rounded-2xl dark:bg-[#313131] bg-stone-200 my-1"></div>
-                <div className="group z-10 relative w-2/6 h-[10px] animate-smooth-pulse rounded-2xl dark:bg-[#313131] bg-stone-200 my-1"></div>
+                <div className="group z-10 relative w-4 h-[10px] animate-smooth-pulse rounded-2xl dark:bg-[#313131] bg-stone-200 my-1"></div>
+                <div className="group z-10 relative w-4 h-[10px] animate-smooth-pulse rounded-2xl dark:bg-[#313131] bg-stone-200 my-1"></div>
               </>
             ) : (
               <>
