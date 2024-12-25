37fe6f24f9c6eeddb4037aec4f1fddecd088ceb8
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 16:51:15 +0200

    updated src/pages/UpfrontAi.jsx

--- src/pages/UpfrontAi.jsx
+++ src/pages/UpfrontAi.jsx
@@ -95,7 +95,7 @@ function UpfrontAi() {
         <h4>Coming soon..</h4>
         <br />
         <div class="flex flex-col gap-2 max-w-[700px] mx-auto">
-          <h1 className="text-lg font-bold">🚀 Key Features</h1>
+          <h1 className="text-lg font-bold mb-2">🚀 Key Features</h1>
           <ol className="flex flex-col gap-3 text-sm">
             <li className="font-normal text-text-color/70 dark:text-[#e2e2e2]/70">
               <strong className="font-medium text-text-color dark:text-[#e2e2e2]">
