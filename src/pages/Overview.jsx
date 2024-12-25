168465226a12949afb012b9c67601c045ffa76aa
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 11:18:11 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -200,6 +200,9 @@ function Projects() {
                         <LuHash />
                       </div>
                     </div>
+                    <h1 className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full max-w-[900px] mx-auto px-5 mt-10 text-sm">
+                      You have no recent activity!
+                    </h1>
                   </Link>
                 ))}
               </div>
