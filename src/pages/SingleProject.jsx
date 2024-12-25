ac522bd75e09884326b80be81f6ad9b822e67c28
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 16:56:46 +0200

    updated src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -935,7 +935,7 @@ function SingleProject() {
             <img
               src={logoSvg}
               loading="lazy"
-              className="h-16 saturate-100 aspect-square"
+              className="h-12 saturate-100 aspect-square"
             />
             <Loader />
           </div>
