50f9af83960b5daed9c05c3fddb07d6f15bea5b5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 16:30:04 +0200

    updated src/pages/UpfrontAi.jsx

--- src/pages/UpfrontAi.jsx
+++ src/pages/UpfrontAi.jsx
@@ -88,7 +88,7 @@ function UpfrontAi() {
                 className={`w-[calc(100%-256px)] h-full min-h-svh flex-1 text-text-color dark:text-[#e2e2e2] text-5xl bg-white dark:bg-dark-body transition-all duration-500 ease-in-out z-10 overflow-clip flex items-center justify-start flex-col px-5 py-20 max-lg:py-8`}
             >
                 {/* title */}
-                <h1 className="">Upfront <span className="font-Custom_1xb ">Ai</span></h1>
+                <h1 className="">Upfront <span className="font-Custom_1xb ">AI</span></h1>
             </div>
         </div>
     );
