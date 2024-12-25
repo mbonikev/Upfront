43e20727c5b9e4ac1157e2d3aecffa58866f4a77
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 11:28:54 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -206,7 +206,7 @@ function Projects() {
                     <div className="flex-1 w-full flex items-end justify-start gap-1">
                       {project.collaborations.map((collab, index) => (
                         <h1 key={index} className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full max-w-[900px] mx-auto px-3 mt-[28px] line-clamp-2 text-sm">
-                          <span className="h-[23px] w-[23px] min-w-[23px] aspect-square flex items-center justify-center rounded-full dark:bg-[#424242] text-[#979797]">{collab.charAt(0)}</span>
+                          <span className="h-[23px] w-[23px] min-w-[23px] aspect-square flex items-center justify-center rounded-full dark:bg-[#424242] text-[#979797] capitalize font-medium">{collab.charAt(0)}</span>
                         </h1>
                       ))}
                     </div>
