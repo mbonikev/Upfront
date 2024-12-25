65add04973ca8b38ad0f7505a910da11866f6884
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 12:00:00 +0200

    updated src/components/Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import logoSvg from "../assets/logo.svg";
 
 function Loader() {
-  return (
-    <div className="fixed top-0 z-50 left-0 w-full h-full bg-white dark:bg-dark-body flex items-center justify-center flex-col gap-3">
-      <img
-        src={logoSvg}
-        loading="lazy"
-        className="h-16 saturate-100 aspect-square"
-      />
-      <span class="loader"></span>
-    </div>
-  );
+  return <span class="loader"></span>;
 }
 
 export default Loader;
