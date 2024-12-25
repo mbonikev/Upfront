f27633e2eb5e37b5d66fc318d5e782fe181bb843
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 09:25:37 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -143,7 +143,7 @@ function Projects() {
         setW1={setW1}
       />
       <div
-        className={`w-full h-full min-h-svh flex-1 text-text-color bg-stone-50 dark:bg-dark-body transition-all duration-500 ease-in-out z-10 flex items-start justify-start`}
+        className={`w-full h-full min-h-svh flex-1 text-text-color bg-stone-50 dark:bg-dark-body transition-all duration-500 ease-in-out z-10 overflow-clip flex items-start justify-start`}
       >
         <div className="relative w-auto flex-1 h-full max-w-[2300px] mx-auto px-7 max-xl:px-0">
           {/* Left Button */}
