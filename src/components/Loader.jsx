fd4e2925bc0260d44d8635ff3881337d0264195c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 11:57:49 +0200

    updated src/components/Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <div className="fixed top-0 z-50 left-0 w-full h-full bg-white dark:bg-dark-body flex items-center justify-center flex-col gap-3">
       <img
-        src={}
+        src={logoSvg}
         loading="lazy"
         className="h-16 saturate-100 aspect-square"
       />
