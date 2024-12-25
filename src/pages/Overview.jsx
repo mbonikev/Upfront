72501596e0ecbb2be4c0f40451ccfbce8b38f435
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 15:29:00 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -211,17 +211,17 @@ function Projects() {
                       {project.name !== "" ? project.name : "Untitled"}
                     </h1>
                     <div className="flex-1 pb-2 px-3 w-full flex items-end justify-start gap-1">
-                      {project.collaborations.map((collab, index) => (
-                        <h1
-                          key={index}
-                          className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full text-xs flex items-center justify-start gap-1.5"
-                        >
-                          <span className="h-[22px] w-[22px] min-w-[22px] aspect-square flex items-center justify-center rounded-full bg-main-color text-white dark:bg-[#424242] dark:text-[#b8b8b8] capitalize font-semibold">
+                      <h1 className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full text-xs flex items-center justify-start gap-1.5">
+                        {project.collaborations.map((collab, index) => (
+                          <span
+                            key={index}
+                            className="h-[22px] w-[22px] min-w-[22px] aspect-square flex items-center justify-center rounded-full bg-main-color text-white dark:bg-[#424242] dark:text-[#b8b8b8] capitalize font-semibold"
+                          >
                             {collab.charAt(0)}
                           </span>
-                          <span className="text-xs opacity-85">2d ago</span>
-                        </h1>
-                      ))}
+                        ))}
+                        <span className="text-xs opacity-85">2d ago</span>
+                      </h1>
                       <h1 className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full text-xs flex items-center justify-start gap-1.5">
                         <span className="h-[22px] w-[22px] min-w-[22px] aspect-square flex items-center justify-center rounded-full bg-main-color text-white dark:bg-[#424242] dark:text-[#b8b8b8] capitalize font-semibold">
                           {project.collaborations.length}
