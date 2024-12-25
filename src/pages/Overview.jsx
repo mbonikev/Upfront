57aaddb76234c8bd75c629def6ac19308351864f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-25 11:25:03 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -204,8 +204,9 @@ function Projects() {
                       {project.name}
                     </h1>
                     <div className="flex-1 w-full flex items-end justify-start gap-1">
+                      {project.collaborations}
                       <h1 className="text-dark-body/50 dark:text-[#b8b8b8]/80 font-normal w-full max-w-[900px] mx-auto px-3 mt-[28px] line-clamp-2 text-sm">
-                        {project.name}
+                        {}
                       </h1>
                     </div>
                   </Link>
