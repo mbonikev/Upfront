584e08540727628d0f8d356df6201588421ccc01
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 10:06:58 +0200

    updated src/utils/ProtectiveRoutes.jsx

--- src/utils/ProtectiveRoutes.jsx
+++ src/utils/ProtectiveRoutes.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 const ProtectedRoutes = () => {
-    const [user, setUser] = useState(null); // Initialize with null
-    const [username, setUserName] = useState(null); // Initialize with null
-    const [userEmail, setUserEmail] = useState(null)
-    useEffect(() => {
-        const email = localStorage.getItem('upfront_user');
-        const name = localStorage.getItem('upfront_user_name');
-        if (email) {
-            setUser(true);
-            setUserName(name)
-            setUserEmail(email)
-        } else {
-            setUser(false);
-            setUserName(null)
-            setUserEmail(null)
-        }
-    }, []);
-    if (user === null) {
-        return <></>;
+  const [user, setUser] = useState(null);
+  const [username, setUserName] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+  useEffect(() => {
+    const email = localStorage.getItem("upfront_user");
+    const name = localStorage.getItem("upfront_user_name");
+    const workspaces = JSON.parse(localStorage.getItem("upfront_ws")) || [];
+    if (email) {
+      setUser(true);
+      setUserName(name);
+      setUserEmail(email);
+      setWorkspaces(workspaces);
+    } else {
+      setUser(false);
+      setUserName(null);
+      setUserEmail(null);
     }
-    return user ? <Outlet context={{ username, userEmail }} /> : <Navigate to={"/auth/login"} />;
+  }, []);
+  if (user === null) {
+    return <></>;
+  }
+  return user ? (
+    <Outlet context={{ username, userEmail }} />
+  ) : (
+    <Navigate to={"/auth/login"} />
+  );
 };
 export default ProtectedRoutes;
