62b59dbc5521b313b01ea12a56f0e7b3bfafe52f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 14:14:44 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -88,7 +88,7 @@ function Sidebar({
   const [newWorkspaceName, setNewWorkspaceName] = useState("");
   const [creatingWps, setCreatingWps] = useState(false);
   const [deletingWps, setDeletingWps] = useState(false);
-  const [collapseWorkspaces, setCollapseWokspaces] = useState(false)
+  const [collapseWorkspaces, setCollapseWorkspaces] = useState(false)
   // workspace1
   const handleUpdateWorkSpace = async (e) => {
     e.preventDefault();
