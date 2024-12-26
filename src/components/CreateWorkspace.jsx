a013bebe7be8791e0019ecfbeae9d9906f3305de
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:58:08 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -15,7 +15,7 @@ function CreateWorkspace() {
           <h1 className="font-normal text-sm">Workspace Name</h1>
           <input
             type="text"
-            className="w-full h-[30px] rounded-lg px-3 ring-1 ring-stone-200"
+            className="w-full h-[30px] rounded-lg px-3 ring-1 ring-stone-200 focus:ring-2 focus:ring-main-color/60"
           />
         </div>
         <div className="flex justify-end items-center gap-2 px-2">
