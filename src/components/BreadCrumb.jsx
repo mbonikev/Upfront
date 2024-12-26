49c365f08ee9504a93e6922169fc542cb3573972
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 12:53:57 +0200

    updated src/components/BreadCrumb.jsx

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-function BreadCrumb({name ,link ,status}) {
+import React from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+
+function BreadCrumb({ name, link, status }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = (e) => {
+    if (location.pathname === link) {
+      e.preventDefault();
+      navigate(link, { replace: true }); // Force navigation to the same path
+    }
+  };
+
   return (
     <>
-        {status === 'on' ? (
-            <Link className='py-1 px-[6px] text-xs font-medium hover:bg-stone-100 dark:text-[#b8b8b8]/70 dark:hover:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] rounded-md text-text-color/70 cursor-pointer whitespace-nowrap max-w-[140px] truncate' to={link}>{name}</Link>
-        ) : (
-            <button className='py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default whitespace-nowrap dark:text-[#b8b8b8]/70 max-w-[140px] truncate'>{name}</button>
-        )}
+      {status === "on" ? (
+        <Link
+          className="py-1 px-[6px] text-xs font-medium hover:bg-stone-100 dark:text-[#b8b8b8]/70 dark:hover:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] rounded-md text-text-color/70 cursor-pointer whitespace-nowrap max-w-[140px] truncate"
+          to={link}
+          onClick={handleClick}
+        >
+          {name}
+        </Link>
+      ) : (
+        <button className="py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default whitespace-nowrap dark:text-[#b8b8b8]/70 max-w-[140px] truncate">
+          {name}
+        </button>
+      )}
     </>
-  )
+  );
 }
-export default BreadCrumb
+
+export default BreadCrumb;
