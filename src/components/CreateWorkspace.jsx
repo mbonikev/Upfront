a35728e09da96e40d97db79cdee967ea5f785ba2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:51:07 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { LuX } from 'react-icons/lu'
 
 function CreateWorkspace() {
   return (
     <div className='w-[300px]'>
       <div className='w-full flex items-center justify-between gap-1 p-2 border-b border-stone-200 dark:border-[#474747] text-sm'>
         <h1 className='font-semibold'>Create Workspace</h1>
-        
+        <button className='h-[50px] w-[50px] aspect-square flex items-center justify-center'>
+          <LuX />
+        </button>
       </div>
     </div>
   )
