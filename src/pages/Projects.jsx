a2e4dcc5fde86ec27b255fdc01723867dca98779
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:35:55 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -84,7 +84,7 @@ function Projects() {
         const response = await axios.get(`${apiUrl}/api/getthisworkspace`, {
           params: { userEmail, workspaceId },
         });
-        console.log(response);
+        console.log(response.data);
       } catch (error) {
         if (error.response.status === 400 || error.response.status === 401) {
           navigate("/");
