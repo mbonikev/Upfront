818f0b9e227545a8444686b632dd7bd66a9d05db
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:49:37 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function CreateWorkspace() {
   return (
-    <div className='w-[300px]'></div>
+    <div className='w-[300px]'>
+      <div className='w-full flex items-center justify-between gap-1 px-2'></div>
+    </div>
   )
 }
 
