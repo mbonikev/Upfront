d295bb2b12013e2e91afc77427c26931fdd53d97
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 17:14:47 +0200

    updated src/pages/SingleProject.jsx

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -103,6 +103,8 @@ function SingleProject() {
   const [newBoardValue, setNewBoardValue] = useState("");
   const [boards, setBoards] = useState([]);
   const [tasks, setTasks] = useState([]);
+  const [boardsAi, setBoardsAi] = useState([]);
+  const [tasksAi, setTasksAi] = useState([]);
   const [placement, SetPlacement] = useState("bottomLeft");
   const [createNewTask, setCreateNewTask] = useState("");
   // Scolling horizonaly
@@ -134,16 +136,16 @@ function SingleProject() {
       });
 
       // boards only
-      if (generateType === "Boards Only") {
-        setAiBoards(response.data.boards);
-        setBoards((prevBoards) => [
-          ...prevBoards,
-          ...response.data.boards.map((board, index) => ({
-            id: board._id,
-            name: board.name.replace(/\*\*/g, "").trim(),
-          })),
-        ]);
-      }
+      // if (generateType === "Boards Only") {
+      //   setAiBoards(response.data.boards);
+      //   setBoards((prevBoards) => [
+      //     ...prevBoards,
+      //     ...response.data.boards.map((board, index) => ({
+      //       id: board._id,
+      //       name: board.name.replace(/\*\*/g, "").trim(),
+      //     })),
+      //   ]);
+      // }
 
       // Boards & Tasks
       if (generateType === "Boards & Tasks") {
