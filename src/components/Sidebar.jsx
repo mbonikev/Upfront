12311fca7c968fd052b8d6bc8be6aa4378d9b3ab
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 10:26:31 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -655,7 +655,7 @@ function Sidebar({
           {/* 3 */}
           <div className="w-full h-fit flex flex-col items-center justify-between p-2 gap-[3px] sticky bottom-0 z-20 bg-white dark:bg-[#202020]">
             {/* shadow */}
-            <div className="w-full h-[40px] absolute top-[-37px] left-0 bg-gradient-to-t from-white via-white dark:via-[#202020] dark:from-[#202020] to-transparent"></div>
+            <div className="w-full h-[30px] absolute top-[-27px] left-0 bg-gradient-to-t from-white via-white dark:via-[#202020] dark:from-[#202020] to-transparent"></div>
             <button
               className={`${linkStyle} hover:bg-stone-100 group-hover:bg-stone-100 outline-none`}
             >
