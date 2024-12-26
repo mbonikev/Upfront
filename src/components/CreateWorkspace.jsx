cbc9409311e8bc005034f422608bd74cac0d755c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:57:18 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -4,14 +4,14 @@ import { LuX } from "react-icons/lu";
 function CreateWorkspace() {
   return (
     <div className="w-[300px] flex flex-col gap-2">
-      <div className="w-full flex items-center justify-between gap-1 py-2 px-3 border-b border-stone-200 dark:border-[#474747] text-sm">
+      <div className="w-full flex items-center justify-between gap-1 p-2 border-b border-stone-200 dark:border-[#474747] text-sm">
         <h1 className="font-semibold">Create Workspace</h1>
         <button className="h-[25px] w-[25px] rounded-lg text-base hover:bg-stone-100 dark:hover:bg-[#383838] aspect-square flex items-center justify-center">
           <LuX />
         </button>
       </div>
       <form className="flex-1 flex flex-col gap-0 pb-2">
-        <div className="flex flex-col gap-2 px-3 pt-2">
+        <div className="flex flex-col gap-2 px-2 pt-2">
           <h1 className="font-normal text-sm">Workspace Name</h1>
           <input
             type="text"
