95bbae2712c2e9968a68d92504d4c63ce8f50437
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:50:37 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -5,6 +5,7 @@ function CreateWorkspace() {
     <div className='w-[300px]'>
       <div className='w-full flex items-center justify-between gap-1 p-2 border-b border-stone-200 dark:border-[#474747] text-sm'>
         <h1 className='font-semibold'>Create Workspace</h1>
+        
       </div>
     </div>
   )
