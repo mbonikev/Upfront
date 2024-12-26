e9617fb83776e81c2ac5d091a8faff51f7cd9528
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 09:00:49 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -15,6 +15,7 @@ function CreateWorkspace() {
           <h1 className="font-normal text-sm">Workspace Name</h1>
           <input
             type="text"
+            autoFocus={true}
             className="w-full h-[30px] rounded-lg px-3 ring-1 ring-stone-200 focus:ring-2 focus:ring-main-color/60 transition bg-stone-100 dark:bg-[#404040] dark:ring-transparent "
           />
         </div>
