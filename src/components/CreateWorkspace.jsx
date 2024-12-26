cd0c62eaafa3571bb8193b34d4b7567267156643
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 09:03:19 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -10,7 +10,7 @@ function CreateWorkspace() {
           <LuX />
         </button>
       </div>
-      <form className="flex-1 flex flex-col gap-0 pb-0 ">
+      <form className="flex-1 flex flex-col gap-4 pb-0 ">
         <div className="flex flex-col gap-2 px-2 pt-2">
           <h1 className="font-normal text-sm">Workspace Name</h1>
           <input
