0f4e84238b06348630364c823350c8933c7b2e6e
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-26 08:53:18 +0200

    updated src/components/CreateWorkspace.jsx

--- src/components/CreateWorkspace.jsx
+++ src/components/CreateWorkspace.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { LuX } from 'react-icons/lu'
+import React from "react";
+import { LuX } from "react-icons/lu";
 
 function CreateWorkspace() {
   return (
-    <div className='w-[300px] flex flex-col gap-2'>
-      <div className='w-full flex items-center justify-between gap-1 p-2 border-b border-stone-200 dark:border-[#474747] text-sm'>
-        <h1 className='font-semibold'>Create Workspace</h1>
-        <button className='h-[25px] w-[25px] rounded-lg text-base hover:bg-stone-100 dark:hover:bg-[#383838] aspect-square flex items-center justify-center'>
+    <div className="w-[300px] flex flex-col gap-2">
+      <div className="w-full flex items-center justify-between gap-1 p-2 border-b border-stone-200 dark:border-[#474747] text-sm">
+        <h1 className="font-semibold">Create Workspace</h1>
+        <button className="h-[25px] w-[25px] rounded-lg text-base hover:bg-stone-100 dark:hover:bg-[#383838] aspect-square flex items-center justify-center">
           <LuX />
         </button>
       </div>
-      <form className='flex-1 flex flex-col gap-2'>
-        <div className='flex flex-col gap-2 p-2'>
-          <h1></h1>
+      <form className="flex-1 flex flex-col gap-2">
+        <div className="flex flex-col gap-2 p-2">
+          <h1 className="font-normal">Workspace Name</h1>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default CreateWorkspace
+export default CreateWorkspace;
