0cc4816fe5335126245dc01c8132e189891d65d2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-27 17:40:36 +0200

    updated src/pages/Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -34,6 +34,7 @@ function Projects() {
     localStorage.removeItem("upfront_user_name");
     localStorage.removeItem("upfront_ws");
     localStorage.removeItem("mycollaborations");
+    localStorage.removeItem("collapseWorkspaces");
     setTimeout(() => {
       window.location.reload();
     }, 1000);
@@ -139,7 +140,12 @@ function Projects() {
           },
         }}
       />
-      <Sidebar username={username} userEmail={userEmail} workspaces={workspaces} setWorkspaces={setWorkspaces} />
+      <Sidebar
+        username={username}
+        userEmail={userEmail}
+        workspaces={workspaces}
+        setWorkspaces={setWorkspaces}
+      />
       <div
         className={`w-[calc(100%-256px)] h-full min-h-svh flex-1 text-text-color bg-white dark:bg-dark-body transition-all duration-500 ease-in-out z-10 overflow-clip flex items-start justify-start flex-col p-1`}
       >
