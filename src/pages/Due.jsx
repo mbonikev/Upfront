0011735df835d68c93dba6c6c250bd96e10bc029
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 20:22:15 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -155,15 +155,15 @@ function Due() {
                     </h1>
                   </div>
                 </div>
-                <div className="flex flex-col w-full h-full px-1 py-2 ">
-                  <div className="w-full flex-1 flex pr-4">
+                <div className="flex flex-col w-full h-full px-1 py-2 text-text-color dark:text-light-text-color">
+                  <div className="w-full flex-1 flex pr-3">
                     <div className="w-full flex min-h-full gap-2 items-start justify-start">
                       {/* line */}
                       <div className="w-[4px] min-w-[4px] h-full rounded-xl bg-stone-200/90 flex"></div>
                       {/* task content */}
                       <div className="w-full flex items-center justify-between py-1">
                         <span>Define Curriculum</span>
-                        <span className="text-sm">2 days ago</span>
+                        <span className="text-xs opacity-75">2 days ago</span>
                       </div>
                     </div>
                   </div>
