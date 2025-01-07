1342ef02cc25cebd23a61c789fe97e1a51b19c06
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 19:58:44 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -141,8 +141,8 @@ function Due() {
           {/* main */}
           <div className="w-full h-fit px-6">
             {/* task */}
-            <div className="w-full min-h-[200px] flex items-start justify-start flex-col gap-2 ring-1 ring-stone-200/60 dark:ring-[#282828] rounded-2xl p-1">
-              <div className="w-full h-full flex gap-2">
+            <div className="w-full flex items-start justify-start flex-col gap-2 ring-1 ring-stone-200/60 dark:ring-[#282828] rounded-2xl p-1">
+              <div className="w-full h-full min-h-[140px] flex gap-2">
                 <div className="w-[140px] bg-[#fb923c60] text-text-color/95 rounded-xl p-2 flex">
                   <div className="w-full h-fit bg-white dark:bg-dark-body dark:text-light-text-color rounded-lg flex items-center justify-center flex-col gap-0 p-3">
                     <h1 className="font-semibold">Dec</h1>
