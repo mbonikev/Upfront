e9c8039269423cc00ff9feb03c27ce653d2e4b12
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 20:20:45 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -157,11 +157,11 @@ function Due() {
                 </div>
                 <div className="flex flex-col w-full h-full px-1 py-2 ">
                   <div className="w-fit flex-1 flex">
-                    <div className="flex min-h-full gap-2 items-start justify-start py-3">
+                    <div className="flex min-h-full gap-2 items-start justify-start">
                       {/* line */}
                       <div className="w-[4px] min-w-[4px] h-full rounded-xl bg-stone-200/90 flex"></div>
                       {/* task content */}
-                      <div className="w-full flex items-center justify-between">
+                      <div className="w-full flex items-center justify-between py-1">
                         <span>Define Curriculum</span>
                         <span></span>
                       </div>
