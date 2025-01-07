8a34ff65a442029a2b12cc36d538f83c317127ae
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 19:55:13 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -141,16 +141,24 @@ function Due() {
           {/* main */}
           <div className="w-full h-fit px-6">
             {/* task */}
-            <div className="w-full h-full flex items-start justify-start gap-2 ring-1 ring-stone-100 dark:ring-[#282828] rounded-2xl p-2">
-                <div className="w-[140px] h-full bg-[#fb923c60] text-text-color/95 rounded-xl p-2 flex">
-                    <div className="w-full h-fit bg-white rounded-lg flex items-center justify-center flex-col gap-0 p-3">
-                        <h1 className="font-semibold">Dec</h1>
-                        <h1 className="text-4xl font-semibold tracking-tight">23</h1>
-                    </div>
+            <div className="w-full h-fit flex items-start justify-start gap-2 ring-1 ring-stone-100 dark:ring-[#282828] rounded-2xl p-2">
+              <div className="w-[140px] h-full bg-[#fb923c60] text-text-color/95 rounded-xl p-2 flex">
+                <div className="w-full h-fit bg-white rounded-lg flex items-center justify-center flex-col gap-0 p-3">
+                  <h1 className="font-semibold">Dec</h1>
+                  <h1 className="text-4xl font-semibold tracking-tight">23</h1>
                 </div>
-                <div className="flex-1 w-full h-fit bg-red-300">ee <br />ee <br />ee <br />ee <br />ee <br />ee <br />ee <br />
-                </div>
+              </div>
+              <div className="flex-1 w-full h-fit bg-red-300">
+                ee <br />
+                ee <br />
+                ee <br />
+                ee <br />
+                ee <br />
+                ee <br />
+                ee <br />
+              </div>
             </div>
+
             <div></div>
             {/* no due */}
             {/* <h1 className="text-dark-body/50 dark:text-light-text-color/80 font-normal max-w-[500px] break-words text-sm">
