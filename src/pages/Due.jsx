381f1b8fb6afcd68c445ee29f6296a6161c1b460
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 20:08:14 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -146,8 +146,8 @@ function Due() {
                 <div className="w-[140px] bg-stone-100 dark:bg-[#282828] text-text-color/95 rounded-xl p-2 flex">
                   <div className="w-full h-fit bg-white dark:bg-dark-body shadow-lg dark:text-light-text-color rounded-lg flex items-center justify-center flex-col gap-0 p-3 relative">
                     <div className="w-full h-fit absolute top-[-5px] px-3 flex items-center justify-between">
-                        <div className="w-[4px] h-[12px] bg-stone-200/70 dark:bg-[#383838] rounded-lg"></div>
-                        <div className="w-[4px] h-[12px] bg-stone-200/70 dark:bg-[#383838] rounded-lg"></div>
+                        <div className="w-[4px] h-[12px] bg-stone-200/90 dark:bg-[#383838] rounded-lg"></div>
+                        <div className="w-[4px] h-[12px] bg-stone-200/90 dark:bg-[#383838] rounded-lg"></div>
                     </div>
                     <h1 className="font-semibold">Dec</h1>
                     <h1 className="text-4xl font-semibold tracking-tight">
