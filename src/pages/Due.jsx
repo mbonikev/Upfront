ca001c89358d7526a77bab1863cdb84055964bd7
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 19:19:42 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -107,7 +107,7 @@ function Due() {
           <h1 className="text-2xl font-extrabold tracking-normal max-w-[500px] break-words dark:text-[#d4d4d4]">
             Dues
           </h1>
-          <div className="w-full h-fit flex items-center justify-between border-b border-stone-100 dark:border-[#303030] pb-1">
+          <div className="w-full h-fit my-2 flex items-center justify-between border-b border-stone-100 dark:border-[#232323] pb-1">
             <div className="w-fit flex items-center justify-start gap-1">
               <button
                 className={`${linkStyle} text-text-color dark:text-light-text-color bg-stone-100 dark:bg-[#2c2c2c]`}
