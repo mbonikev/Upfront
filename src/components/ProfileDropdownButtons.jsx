702809f86274e2063278b5615b46883e2bbceccd
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 18:15:09 +0200

    updated src/components/ProfileDropdownButtons.jsx

--- src/components/ProfileDropdownButtons.jsx
+++ src/components/ProfileDropdownButtons.jsx
@@ -26,28 +26,28 @@ function ProfileDropdownButtons({ username, userEmail }) {
     }, 1000);
   };
 
-  const linkStyle = `h-[32px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-[#b8b8b8] hover:bg-stone-100 dark:hover:bg-[#383838] text-sm text-text-color tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#404040]`;
+  const linkStyle = `h-[32px] w-full flex items-center gap-2 px-2 py-[7px] font-normal dark:text-light-text-color hover:bg-stone-100 dark:hover:bg-[#383838] text-sm text-text-color tracking-tight rounded-lg line-clamp-1 relative select-none active:bg-stone-100 dark:active:bg-[#404040]`;
 
   return (
     <div className="w-full flex flex-col justify-start items-start bg-white dark:bg-[#2c2c2c]">
       <div className="p-1 flex flex-col w-full">
         <Link to={"/"} className={`${linkStyle}`}>
-          <LuSettings className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <LuSettings className="text-xl text-text-color/50 dark:text-light-text-color/50  min-w-fit" />
           <p className="line-clamp-1">Settings</p>
         </Link>
         <Link to={"/"} className={`${linkStyle}`}>
-          <LuTrash2 className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <LuTrash2 className="text-xl text-text-color/50 dark:text-light-text-color/50  min-w-fit" />
           <p className="line-clamp-1">Trash</p>
         </Link>
       </div>
       <div className="w-full h-[1px] bg-border-line-color/70 dark:bg-[#383838]"></div>
       <div className="p-1 flex flex-col w-full">
         <Link to={"/"} className={`${linkStyle}`}>
-          <LuInfo className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <LuInfo className="text-xl text-text-color/50 dark:text-light-text-color/50  min-w-fit" />
           <p className="line-clamp-1">About Upfront</p>
         </Link>
         <Link to={"/"} className={`${linkStyle}`}>
-          <HiOutlineSpeakerphone className="text-xl text-text-color/50 dark:text-[#b8b8b8]/50  min-w-fit" />
+          <HiOutlineSpeakerphone className="text-xl text-text-color/50 dark:text-light-text-color/50  min-w-fit" />
           <p className="line-clamp-1">Updates</p>
         </Link>
       </div>
