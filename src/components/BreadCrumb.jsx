36960b78adbde71f4c14b6c6a05519d33303ef31
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 18:15:39 +0200

    updated src/components/BreadCrumb.jsx

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -16,14 +16,14 @@ function BreadCrumb({ name, link, status }) {
     <>
       {status === "on" ? (
         <Link
-          className="py-1 px-[6px] text-xs font-medium hover:bg-stone-100 dark:text-[#b8b8b8]/70 dark:hover:text-[#b8b8b8] dark:hover:bg-[#2c2c2c] rounded-md text-text-color/70 cursor-pointer whitespace-nowrap max-w-[140px] truncate"
+          className="py-1 px-[6px] text-xs font-medium hover:bg-stone-100 dark:text-light-text-color/70 dark:hover:text-light-text-color dark:hover:bg-[#2c2c2c] rounded-md text-text-color/70 cursor-pointer whitespace-nowrap max-w-[140px] truncate"
           to={link}
           onClick={handleClick}
         >
           {name}
         </Link>
       ) : (
-        <button className="py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default whitespace-nowrap dark:text-[#b8b8b8]/70 max-w-[140px] truncate">
+        <button className="py-1 px-[6px] text-xs font-medium text-text-color/70 cursor-default whitespace-nowrap dark:text-light-text-color/70 max-w-[140px] truncate">
           {name}
         </button>
       )}
