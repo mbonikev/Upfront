f2e59bd50ae696f73f4b139617c48a637628803e
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-07 20:45:46 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -159,7 +159,7 @@ function Due() {
                   {[...Array(4)].map((_, index) => (
                     <div
                       key={index}
-                      className="w-full flex-1 flex pr-3 bg-[#1f1f1f] rounded-xl p-2"
+                      className="w-full flex-1 flex pr-3 bg-stone-50 dark:bg-[#1f1f1f] rounded-xl p-2"
                     >
                       <div className="w-full flex min-h-full gap-2 items-start justify-start">
                         {/* line */}
