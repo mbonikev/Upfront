9e1172760925d51e082d478612a0910b213b986f
mbonikev <mbonimpayekevin@gmail.com>
2025-01-08 20:49:18 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -142,7 +142,7 @@ function Due() {
           <div className="w-full h-fit px-6">
             {/* task */}
             <div className="w-full max-w-[1200px] flex items-start justify-start flex-col ring-1 ring-stone-200/60 dark:ring-[#282828] rounded-2xl p-1">
-              <div className="w-full h-full min-h-[140px] flex gap-1">
+              <div className="w-full h-full flex gap-1">
                 <div className="w-[140px] bg-stone-100 dark:bg-[#282828] text-text-color/95 rounded-xl p-2 flex">
                   <div className="w-full h-fit bg-white dark:bg-dark-body shadow-lg dark:text-light-text-color rounded-lg flex items-center justify-center flex-col gap-0 p-3 relative">
                     <div className="w-full h-fit absolute top-[-5px] px-3 flex items-center justify-between">
