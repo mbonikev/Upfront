3a8a9e94fc2662e30aad9d0d2268a19cd503d9ae
mbonikev <mbonimpayekevin@gmail.com>
2025-01-29 20:32:31 +0200

    updated src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -26,15 +26,15 @@ function SearchModal({ Hide }) {
       <div className="w-full h-[1px] bg-[#353535] my-3"></div>
       <div className="w-full flex-1 flex items-start justify-start gap-5">
         <div className="w-1/3 h-full flex items-start justify-start flex-col gap-0.5">
-          <button className="outline-none flex items-center justify-start bg-[#505050] dark:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+          <button className="outline-none flex items-center justify-start bg-[#505050] dark:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-sm font-medium ">
             <LuHash className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Projects</span>
           </button>
-          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-sm font-medium ">
             <FiCheckCircle className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Tasks</span>
           </button>
-          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
+          <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-sm font-medium ">
             <PiPersonSimpleRunBold className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Templates</span>
           </button>
