f13bde980e37b6689f47a731bd2254e2d504c90e
mbonikev <mbonimpayekevin@gmail.com>
2025-01-29 20:32:21 +0200

    updated src/components/SearchModal.jsx

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -27,15 +27,15 @@ function SearchModal({ Hide }) {
       <div className="w-full flex-1 flex items-start justify-start gap-5">
         <div className="w-1/3 h-full flex items-start justify-start flex-col gap-0.5">
           <button className="outline-none flex items-center justify-start bg-[#505050] dark:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
-            <LuHash className="text-xl" />
+            <LuHash className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Projects</span>
           </button>
           <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
-            <FiCheckCircle className="text-xl" />
+            <FiCheckCircle className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Tasks</span>
           </button>
           <button className="outline-none flex items-center justify-start hover:bg-[#505050] dark:hover:bg-[#2f2f2f] w-full h-[42px] px-3 rounded-xl gap-2 text-[#a1a1a1] text-[16px] font-medium ">
-            <PiPersonSimpleRunBold className="text-xl" />
+            <PiPersonSimpleRunBold className="text-lg" />
             <span className="text-[#f0f0f0] tracking-tight">Templates</span>
           </button>
         </div>
