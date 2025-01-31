3e6dbab81957ae2a57bf38bf7fd6e6704432efe3
mbonikev <mbonimpayekevin@gmail.com>
2025-01-31 20:21:19 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -393,7 +393,7 @@ function Sidebar({
         {showSearchModal && (
           <div
             onClick={handleHideSearch}
-            className={` top-0 left-0 w-full h-full z-20 dark:bg-black/40 backdrop-blur-[3px] fixed transition-all duration-300 ${
+            className={` top-0 left-0 w-full h-full z-20 dark:bg-black/40 backdrop-blur-[2px] fixed transition-all duration-300 ${
               AnimateShowSearchModal ? "opacity-100 " : "opacity-0"
             }`}
           ></div>
