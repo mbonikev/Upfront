656630349aab4e8dca8aab60843720a264567519
mbonikev <mbonimpayekevin@gmail.com>
2025-01-31 19:12:05 +0200

    updated src/pages/Due.jsx

--- src/pages/Due.jsx
+++ src/pages/Due.jsx
@@ -172,7 +172,7 @@ function Due() {
                           {/* assigned to */}
                           <div className="h-full text-xs flex items-center justify-end gap-2">
                             <div className="ring-1 ring-stone-200/60 dark:ring-[#282828] py-1 px-2.5 text-xs text-light-text-color/60 rounded-full ">
-                              <span className="text-red-600">•</span>
+                              <span className="text-[#ff5630]">•</span>
                               Movie Production
                             </div>
                             <div className="ring-1 ring-stone-200/60 dark:ring-[#282828] py-1 px-2.5 text-xs text-light-text-color/60 rounded-full ">
