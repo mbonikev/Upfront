a6c6d80fc98bbf420f971282ccec07516c8be175
mbonikev <mbonimpayekevin@gmail.com>
2025-01-31 19:35:17 +0200

    updated src/components/Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -393,7 +393,7 @@ function Sidebar({
         {showSearchModal && (
           <div
             onClick={handleHideSearch}
-            className={` top-0 left-0 w-full h-full z-20 bg-black/40 dark:bg-black/40 fixed transition-all duration-300 ${
+            className={` top-0 left-0 w-full h-full z-20 dark:bg-black/40 backdrop-blur-[3px] fixed transition-all duration-300 ${
               AnimateShowSearchModal ? "opacity-100 " : "opacity-0"
             }`}
           ></div>
@@ -401,7 +401,7 @@ function Sidebar({
         {/* search modal */}
         {showSearchModal && (
           <div
-            className={`w-fit h-full max-h-[63%] 2xl:max-h-[500px] bg-white dark:bg-[#252525] rounded-[20px] fixed top-0 left-0 right-0 bottom-0 m-auto shadow-custom ring-1 ring-border-line-color/0 z-30 transition-all duration-150 
+            className={`w-fit h-full max-h-[63%] 2xl:max-h-[500px] bg-[#202020] dark:bg-[#252525] rounded-[25px] fixed top-0 left-0 right-0 bottom-0 m-auto shadow-custom ring-1 ring-border-line-color/0 z-30 transition-all duration-150 
               ${
                 AnimateShowSearchModal
                   ? "opacity-100"
