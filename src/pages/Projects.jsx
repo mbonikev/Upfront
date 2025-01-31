261af94058f7999ce51fa2a913ba4c52941e9e8c
mbonikev <mbonimpayekevin@gmail.com>
2025-01-31 18:50:33 +0200

    updated src/pages/Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -311,7 +311,7 @@ function Projects() {
           ) : (
             <>
               <div className="flex items-center justify-start gap-2 ">
-                <FcFolder className="text-3xl text-text-color/50 dark:text-[#858585]" />
+                <FcFolder className="text-4xl text-text-color/50 dark:text-[#858585]" />
                 <h1 className="text-2xl font-extrabold tracking-normal max-w-[500px] break-words dark:text-[#d4d4d4]">
                   {pageTitle}
                   {/* <span className=" self-end text-xs font-medium bg-teal-600/10 dark:text-light-text-color dark:bg-[#2c2c2c] mb-[4px] ml-2 py-[3px] px-2 tracking-tight rounded-md">
